c466aa1b5b6555d1dcd005fb15984b74712badee
DUMONT Thibaut <thdumont1@airfrance.fr>
2018-04-25 18:09:00 +0200

    EDOSPNC-3 Ajout appel service pour création objectif

--- src/pages/career-objective-create/career-objective-create.ts
+++ src/pages/career-objective-create/career-objective-create.ts
@@ -1,7 +1,8 @@
+import { CareerObjectiveProvider } from './../../providers/career-objective/career-objective';
 import { TranslateService } from '@ngx-translate/core';
 import { CareerObjective } from './../../models/careerObjective';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -13,13 +14,15 @@ export class CareerObjectiveCreatePage {
   creationForm: FormGroup;
   careerObjective: CareerObjective;
 
-  customDateTimeOptions
+  customDateTimeOptions: any;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public formBuilder: FormBuilder,
-    public translateService: TranslateService) {
+    public translateService: TranslateService,
+    private formBuilder: FormBuilder,
+    private careerObjectiveProvider: CareerObjectiveProvider,
+    private toastCtrl: ToastController) {
     this.careerObjective = new CareerObjective();
 
     // Initialisation du formulaire
@@ -31,7 +34,7 @@ export class CareerObjectiveCreatePage {
       managerCommentControl: ['', Validators.maxLength(4000)],
       pncCommentControl: ['', Validators.maxLength(4000)],
       nextEncounterDateControl: [''],
-      urgentControl: ['']
+      urgentControl: [false]
     });
 
     // Options du datepicker
@@ -40,7 +43,7 @@ export class CareerObjectiveCreatePage {
         text: this.translateService.instant('GLOBAL.DATEPICKER.CLEAR'),
         handler: () => this.careerObjective.nextEncounterDate = ''
       }]
-    }
+    };
   }
 
   ionViewDidLoad() {
@@ -51,6 +54,22 @@ export class CareerObjectiveCreatePage {
    * Lance le processus de création d'un objectif
    */
   createCareerObjective() {
-
+    this.careerObjectiveProvider
+      .create(this.careerObjective)
+      .then(createdCareerObjective => {
+        this.toastCtrl.create({
+          message: this.translateService.instant('CAREER_OBJECTIVE_CREATE.SUCCESS.CAREER_OBJECTIVE_CREATED'),
+          duration: 3000,
+          position: 'bottom'
+        }).present();
+      }, error => {
+        console.log(error);
+        this.toastCtrl.create({
+          message: this.translateService.instant('CAREER_OBJECTIVE_CREATE.ERROR.CAREER_OBJECTIVE_CREATION_FAILED'),
+          duration: 30000,
+          position: 'bottom',
+          cssClass: 'error',
+        }).present();
+      });
   }
 }
--- src/providers/career-objective/career-objective.ts
+++ src/providers/career-objective/career-objective.ts
@@ -1,10 +1,28 @@
-import { HttpClient } from '@angular/common/http';
+import { AppConfig } from './../../app/app.config';
+import { RestRequest } from './../../services/rest.base.service';
+import { CareerObjective } from './../../models/careerObjective';
 import { Injectable } from '@angular/core';
+import { RestService } from '../../services/rest.base.service';
 
 @Injectable()
 export class CareerObjectiveProvider {
+  private careerObjectiveUrl: string;
 
-  constructor(public http: HttpClient) {
+  constructor(public restService: RestService) {
+    this.careerObjectiveUrl = `${AppConfig.apiUrl}/career_objectives`;
   }
 
+  /**
+   * Créé un objectif
+   * @param  careerObjective l'objectif à créer
+   * @return une promesse contenant l'objectif créé
+   */
+  create(careerObjective: CareerObjective): Promise<CareerObjective> {
+    let request = new RestRequest();
+    request.method = "POST";
+    request.url = this.careerObjectiveUrl;
+    request.jsonData = careerObjective;
+
+    return this.restService.call(request);
+  }
 }
