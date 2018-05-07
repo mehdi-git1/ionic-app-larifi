2b76e344447b92be0fb2cfef17f230ec8570ca6e
T329354 <remazouari-ext@airfrance.fr>
2018-05-07 17:29:03 +0200

    EDOSPNC-7 home.ts : suppression des impots non utilisés

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,5 @@
 import { RestRequest } from './../../services/rest.base.service';
 import { PncHomePage } from './../pnc-home/pnc-home';
-import { CareerObjectiveCreatePage } from './../career-objective-create/career-objective-create';
-import { CareerObjectiveListPage } from './../career-objective-list/career-objective-list';
 import { Pnc } from './../../models/pnc';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
