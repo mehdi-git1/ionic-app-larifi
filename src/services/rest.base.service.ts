62ce5df4887fd80e0f7a334262e29e0e3e949f4d
Dumont <m414147@airfrance.fr>
2018-06-25 15:56:32 +0200

    EDOSPNC-61 Suppr storeOffline couche basse

--- src/services/rest.base.service.ts
+++ src/services/rest.base.service.ts
@@ -15,29 +15,28 @@ export abstract class RestService {
 
     abstract call(request: RestRequest): Promise<any>;
 
-    get(url: string, jsonData?: any, httpHeaders?: any, storeOffline?: boolean): Promise<any> {
-        return this.sendRequest('GET', url, jsonData, httpHeaders, storeOffline);
+    get(url: string, jsonData?: any, httpHeaders?: any): Promise<any> {
+        return this.sendRequest('GET', url, jsonData, httpHeaders);
     }
 
     post(url: string, jsonData: any, httpHeaders?: any): Promise<any> {
-        return this.sendRequest('POST', url, jsonData, false, httpHeaders);
+        return this.sendRequest('POST', url, jsonData, httpHeaders);
     }
 
     put(url: string, jsonData: any, httpHeaders?: any): Promise<any> {
-        return this.sendRequest('PUT', url, jsonData, false, httpHeaders);
+        return this.sendRequest('PUT', url, jsonData, httpHeaders);
     }
 
     delete(url: string, jsonData?: any, httpHeaders?: any): Promise<any> {
-        return this.sendRequest('DELETE', url, jsonData, false, httpHeaders);
+        return this.sendRequest('DELETE', url, jsonData, httpHeaders);
     }
 
-    sendRequest(method: string, url: string, jsonData: any, httpHeaders?: any, storeOffline?: boolean): Promise<any> {
+    sendRequest(method: string, url: string, jsonData: any, httpHeaders?: any): Promise<any> {
         const request: RestRequest = new RestRequest();
         request.method = method;
         request.url = url;
         request.jsonData = jsonData;
         request.httpHeaders = httpHeaders;
-        request.storeOffline = storeOffline;
 
         return this.call(request);
     }
@@ -49,6 +48,4 @@ export class RestRequest {
     public url: string;
     public httpHeaders: any;
     public jsonData: any;
-    // Défini si on doit stocker le résultat de la requête pour une utilisation offline future
-    public storeOffline = false;
 }
