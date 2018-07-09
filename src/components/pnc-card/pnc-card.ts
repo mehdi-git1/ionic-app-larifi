dd24c4c6e4ab6a84bfbab07ce7554c43e9b93adc
Ludovic CHEMIR <luchemir-ext@airfrance.com>
2018-07-09 13:59:12 +0200

    EDOSPNC-35 : Refactor de l'utilisation de PncCard et mise à jour du OfflineIndicator

--- src/components/pnc-card/pnc-card.ts
+++ src/components/pnc-card/pnc-card.ts
@@ -1,10 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { CrewMember } from '../../models/CrewMember';
 import { SynchronizationProvider } from './../../providers/synchronization/synchronization';
 import { ToastProvider } from './../../providers/toast/toast';
 import { ConnectivityService } from '../../services/connectivity.service';
 import { TranslateService } from '@ngx-translate/core';
 import { GenderProvider } from './../../providers/gender/gender';
+import { PncProvider } from './../../providers/pnc/pnc';
+import { NavController } from 'ionic-angular';
+import { PncHomePage } from './../../pages/pnc-home/pnc-home';
 /**
  * Generated class for the PncCardComponent component.
  *
@@ -15,17 +18,39 @@ import { GenderProvider } from './../../providers/gender/gender';
   selector: 'pnc-card',
   templateUrl: 'pnc-card.html'
 })
-export class PncCardComponent {
+export class PncCardComponent implements AfterViewInit {
 
   @Input() crewMember: CrewMember;
   synchroInProgress: boolean;
 
   constructor(
+    public navCtrl: NavController,
     private genderProvider: GenderProvider,
     public connectivityService: ConnectivityService,
     private synchronizationProvider: SynchronizationProvider,
     private toastProvider: ToastProvider,
-    private translate: TranslateService) {
+    private translate: TranslateService,
+    private pncProvider: PncProvider) {
+  }
+
+  ngAfterViewInit() {
+    this.loadOfflinePncData(this.crewMember.pnc.matricule);
+  }
+
+  /**
+   * charge les données offline du pnc afin de savoir si il est chargé en cache
+   */
+  loadOfflinePncData(matricule): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (this.crewMember.pnc.matricule !== undefined) {
+        this.pncProvider.getPnc(this.crewMember.pnc.matricule).then(foundPnc => {
+          this.crewMember.pnc = foundPnc;
+          resolve();
+        }, error => {
+          reject();
+        });
+      }
+    });
   }
 
   /**
@@ -41,6 +66,10 @@ export class PncCardComponent {
       this.toastProvider.error(this.translate.instant('SYNCHRONIZATION.PNC_SAVED_OFFLINE_ERROR', { 'matricule': matricule }));
       this.synchroInProgress = false;
     });
+  }
+
+  openPncHomePage(matricule) {
+    this.navCtrl.push(PncHomePage, { matricule: matricule });
   }
 
 }
