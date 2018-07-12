0a3fe0d08176d0260b06ecfa3f0235316abfb528
Dumont <m414147@airfrance.fr>
2018-07-12 14:12:15 +0200

    Ajout crewMember

---
+++ src/models/crewMember.ts
@@ -0,0 +1,9 @@
+import { Pnc } from './pnc';
+
+export class CrewMember {
+    pnc: Pnc;
+    particulariy: string;
+    lastEncounterDate: string;
+    onBoardFonction: string;
+    prioritized: boolean;
+}
