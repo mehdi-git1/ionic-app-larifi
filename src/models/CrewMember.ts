89fa0a059613f9676d849fa8c01bb19afdb5e2fe
Dumont <m414147@airfrance.fr>
2018-07-12 14:11:46 +0200

    Suppr CrewMember

--- src/models/CrewMember.ts
+++
@@ -1,8 +0,0 @@
-import { Pnc } from './pnc';
-export class CrewMember {
-    pnc: Pnc;
-    particulariy: string;
-    lastEncounterDate: string;
-    onBoardFonction: string;
-    prioritized: boolean;
-}
