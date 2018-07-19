aba78a62e3f28798e2efbdd8775dc428ebf09376
MARTEL Stéfan <stmartel-ext@airfrance.fr>
2018-07-19 14:40:28 +0200

    Ping sur la partie Web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,7 +48,12 @@ export class EDossierPNC implements OnInit {
   initializeApp() {
     this.platform.ready().then(() => {
 
-      if (!this.connectivityService.isBrowser){
+      /**
+       * Actuellement le ping est configuré pour être effectif sur le web et non sur le mobile
+       * A terme, il faudra le remettre sur le mobile (probléme de CORS à l'eure actuelle)
+       * 
+       */
+      if (this.connectivityService.isBrowser){
         this.connectivityService.pingAPI();
       }
 
