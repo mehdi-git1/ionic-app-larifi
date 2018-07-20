84e337208fff7a0bc390f409402b9b79069cc500
Julien Gloria <julien.gloria@ai3.fr>
2018-07-20 11:18:50 +0200

    Correction merge raté

--- src/pages/flight-crew-list/flight-crew-list.ts
+++ src/pages/flight-crew-list/flight-crew-list.ts
@@ -1,3 +1,5 @@
+import { PncTransformerProvider } from './../../providers/pnc/pnc-transformer';
+import { PncProvider } from './../../providers/pnc/pnc';
 import { SessionService } from './../../services/session.service';
 import { Leg } from './../../models/leg';
 import { PncHomePage } from './../pnc-home/pnc-home';
@@ -5,6 +7,10 @@ import { LegProvider } from './../../providers/leg/leg';
 import { GenderProvider } from './../../providers/gender/gender';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { SynchronizationProvider } from './../../providers/synchronization/synchronization';
+import { ToastProvider } from './../../providers/toast/toast';
+import { ConnectivityService } from '../../services/connectivity.service';
+import { TranslateService } from '@ngx-translate/core';
 import { CrewMember } from '../../models/crewMember';
 
 @Component({
@@ -20,16 +26,30 @@ export class FlightCrewListPage {
     public navParams: NavParams,
     public genderProvider: GenderProvider,
     private legProvider: LegProvider,
-    private sessionService: SessionService) {
+    public connectivityService: ConnectivityService,
+    private toastProvider: ToastProvider,
+    private translate: TranslateService,
+    private pncProvider: PncProvider,
+    private sessionService: SessionService,
+    private pncTransformer: PncTransformerProvider) {
+
   }
+
 
   ionViewCanEnter() {
     this.leg = this.navParams.get('leg');
     this.legProvider.getFlightCrewFromLeg(this.leg.techId).then(flightCrew => {
       this.flightCrewList = flightCrew;
       flightCrew.forEach(crew => {
-        if (crew.pnc.matricule === this.sessionService.authenticatedUser.matricule) {
-          this.sessionService.appContext.onBoardRedactorFonction = crew.onBoardFonction;
+        if (crew.pnc.matricule !== undefined) {
+          if (crew.pnc.matricule === this.sessionService.authenticatedUser.matricule) {
+            this.sessionService.appContext.onBoardRedactorFonction = crew.onBoardFonction;
+          }
+          this.pncProvider.refreshOffLineDateOnPnc(this.pncTransformer.toPnc(crew.pnc)).then(foundPnc => {
+            crew.pnc = foundPnc;
+          }, error => {
+            this.toastProvider.info(this.translate.instant('FLIGHT_CREW_LIST.ERROR', { 'flightNumber': this.leg.number }));
+          });
         }
       });
     }, error => {
