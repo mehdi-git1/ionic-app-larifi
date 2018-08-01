1097f51b04ee7919913415372d798548aca78411
T329354 <remazouari-ext@airfrance.fr>
2018-08-01 16:01:02 +0200

    Mise a jour des fichier de conf des env dev rct et prod

--- src/configuration/environment-variables/prod.ts
+++ src/configuration/environment-variables/prod.ts
@@ -1,15 +1,25 @@
 import { BaseConfig } from './base';
 import { Injectable } from '@angular/core';
 
+declare var window: any;
 @Injectable()
 export class Config extends BaseConfig {
     constructor() {
         super();
         this.appVersion = '1';
-        this.backEndUrl = '/api/other/url';
+
+        if (undefined !== window.cordova && 'browser' !== window.cordova.platformId) {
+            console.log('mobile mode selected');
+            this.backEndUrl = 'https://edospnc-api.airfrance.fr/api/rest/resources';
+            this.pingUrl = this.backEndUrl + '/me';
+        } else {
+            console.log('web mode selected');
+            this.backEndUrl = '/api/rest/resources';
+        }
+
         this.env = 'prod';
-        this.secmobileEnv = 'prd';
-        this.eObsUrl = 'com.airfrance.mobile.inhouse.eformsPNC';
+        this.secmobileEnv = 'prod';
+        this.eObsUrl = 'com.airfrance.mobile.inhouse.eformsrctPNC';
         this.eObsCallbackUrl = 'com.airfrance.mobile.inhouse.edosPNC';
     }
 }
