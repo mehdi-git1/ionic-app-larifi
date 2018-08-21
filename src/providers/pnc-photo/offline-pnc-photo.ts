171e4ce559d136e618f4b244a56e8b6d34b8fc77
Dumont <m414147@airfrance.fr>
2018-08-21 13:07:36 +0200

    EDOSPNC-20 Fix nom import

--- src/providers/pnc-photo/offline-pnc-photo.ts
+++ src/providers/pnc-photo/offline-pnc-photo.ts
@@ -1,7 +1,7 @@
 import { Entity } from './../../models/entity';
 import { StorageService } from './../../services/storage.service';
 import { Injectable } from '@angular/core';
-import { PncPhoto } from '../../models/PncPhoto';
+import { PncPhoto } from '../../models/pncPhoto';
 
 @Injectable()
 export class OfflinePncPhotoProvider {
