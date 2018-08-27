c3f9a256a61ef0f4647fe1b1d25d31c8a9be072e
MARTEL Stéfan <stmartel-ext@airfrance.fr>
2018-08-27 15:23:19 +0200

    EDOSPNC-2 Ecran PINPAD / QUESTION - REPONSE => Finalmisation OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -154,7 +154,9 @@ export class EDossierPNC implements OnInit {
       if (authenticatedUser) {
         this.sessionService.authenticatedUser = authenticatedUser;
         // Gestion de l'affchage du pinPad
-       this.securityModalService.displayPinPad(PinPadType.openingApp);
+        if (!this.secMobilService.isBrowser) {
+          this.securityModalService.displayPinPad(PinPadType.openingApp);
+        }
         this.nav.setRoot(PncHomePage, { matricule: this.sessionService.authenticatedUser.matricule });
       }
       else {
