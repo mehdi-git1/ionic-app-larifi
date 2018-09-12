34d3e8349674453a739d4d0fd0d2cb96346b127a
T778676 <T778676@airfrance.fr>
2018-09-12 15:30:33 +0200

    EDOSPNC-115 Ajout du model des attestations réglementaires

--- src/models/statutoryCertificate.ts
+++ src/models/statutoryCertificate.ts
@@ -1,3 +1,9 @@
+import { VAM } from './statutoryCertificate/vam';
+import { AnnualElearning } from './statutoryCertificate/annualElearning';
+import { MasteringQualification } from './statutoryCertificate/masteringQualification';
+import { FamiliarizationFlight } from './statutoryCertificate/familiarizationFlight';
+import { PlaneSkill } from './statutoryCertificate/planeSkill';
+import { GeneralitySkills } from './statutoryCertificate/generalitySkills';
 
 export class StatutoryCertificate {
     aircraftSkills: Array<string>;
@@ -7,53 +13,4 @@ export class StatutoryCertificate {
     masteringQualification: MasteringQualification;
     annualElearning: AnnualElearning;
     vam: VAM;
-}
-
-class GeneralitySkills{
-    cca: [{
-        libelle: string,
-        startDate: Date,
-        dueDate: Date
-    }];
-    pcb: [{
-        libelle: string,
-        startDate: Date,
-        validityStartDate: Date,
-        dueDate: Date
-    }];
-    gene: [{
-        aircraftSkill: string,
-        startDate: Date,
-        mdcDate: Date,
-        dueDate: Date,
-        ddvDueDate: Date,
-        skillEndDate: Date
-    }];
-}
-
-class PlaneSkill{
-    plane: string;
-    startDate: Date;
-    mdcDate: Date;
-    dueDate: Date;
-    ddvDueDate: Date;
-    skillEndDate: Date;
-}
-
-class FamiliarizationFlight{
-    haulType: HaulType;
-    fa1Date: Date;
-    fa2Date: Date;
-}
-
-enum HaulType{
-    LC= 'STATUTORY_CERTIFICATE.FAMILIARIZATION_FLIGHTS.LC',
-    MC= 'STATUTORY_CERTIFICATE.FAMILIARIZATION_FLIGHTS.MC'
-}
-class MasteringQualification{}
-class AnnualElearning{}
-
-class VAM{
-    validityStartDate: Date;
-    validityEndDate: Date;
 }
---
+++ src/models/statutoryCertificate/generalitySkills.ts
@@ -0,0 +1,21 @@
+export class GeneralitySkills{
+    cca: [{
+        libelle: string,
+        startDate: Date,
+        dueDate: Date
+    }];
+    pcb: [{
+        libelle: string,
+        startDate: Date,
+        validityStartDate: Date,
+        dueDate: Date
+    }];
+    gene: [{
+        aircraftSkill: string,
+        startDate: Date,
+        mdcDate: Date,
+        dueDate: Date,
+        ddvDueDate: Date,
+        skillEndDate: Date
+    }];
+}
