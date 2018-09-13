fd7f289c807685b690544c324ce6f263b6c34cc9
Ludovic CHEMIR <luchemir-ext@airfrance.com>
2018-09-13 17:48:35 +0200

    EDOSPNC-123 Ajout toast d'erreur

--- src/pages/statutory-certificate/statutory-certificate.ts
+++ src/pages/statutory-certificate/statutory-certificate.ts
@@ -1,3 +1,5 @@
+import { TranslateService } from '@ngx-translate/core';
+import { ToastProvider } from './../../providers/toast/toast';
 import { StatutoryCertificate } from './../../models/statutoryCertificate';
 import { StatutoryCertificateProvider } from './../../providers/statutory-certificate/statutory-certificate';
 import { PncProvider } from './../../providers/pnc/pnc';
@@ -24,8 +26,9 @@ export class StatutoryCertificatePage {
     private navParams: NavParams,
     private sessionService: SessionService,
     private pncProvider: PncProvider,
-	private statutoryCertificateProvider: StatutoryCertificateProvider
-  ) {}
+    private statutoryCertificateProvider: StatutoryCertificateProvider,
+    private toastProvider: ToastProvider,
+    private translate: TranslateService) {}
 
   ionViewDidEnter() {
     if (this.navParams.get('matricule')) {
@@ -34,12 +37,15 @@ export class StatutoryCertificatePage {
         this.matricule = this.sessionService.authenticatedUser.matricule;
     }
     if (this.matricule != null) {
-        this.pncProvider.getPnc(this.matricule).then(pnc => {
-            this.pnc = pnc;
-        }, error => {});
-		    this.statutoryCertificateProvider.getStatutoryCertificate(this.matricule).then(statutoryCertificate => {
-          this.statutoryCertificate = statutoryCertificate;
-        }).catch();
+      this.pncProvider.getPnc(this.matricule).then(pnc => {
+          this.pnc = pnc;
+      }, error => {});
+      this.statutoryCertificateProvider.getStatutoryCertificate(this.matricule).then(statutoryCertificate => {
+        this.statutoryCertificate = statutoryCertificate;
+      }).catch(error => {
+        const errorMsg = this.translate.instant('STATUTORY_CERTIFICATE.ERROR.STATUTORY_CERTIFICATE', { 'matricule': this.matricule });
+        this.toastProvider.error(errorMsg);
+      });
     }
   }
 
