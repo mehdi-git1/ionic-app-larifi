ebeab05170e595158bba83703f70702bfd266953
Thibaut Dumont <edossierpnc@MacBook-Pro-de-Thibaut.local>
2018-09-17 15:08:30 +0200

    EDOSPNC-342 Ajout document manuel utilisateur aux ressources d'aide des pnc

--- src/pages/help-asset-list/help-asset-list.ts
+++ src/pages/help-asset-list/help-asset-list.ts
@@ -110,7 +110,14 @@ export class HelpAssetListPage {
      * Renvoie la liste des ressources d'aide du pnc
      */
     getHSTHelpAssets(): HelpAsset[] {
-        return [];
+        const helpAsset = new Array(1);
+        const UserManual = 'Manuel-Utilisateur-PNC-V1.0.pdf';
+        helpAsset[0] = new HelpAsset();
+        helpAsset[0].url = `${this.pdfUrl}/cadre/${UserManual}`;
+        helpAsset[0].label = 'Manuel Utilisateur V1';
+        helpAsset[0].helpAssetType = HelpAssetType.PDF;
+        helpAsset[0].lastUpdateDate = '2018-09-17T10:00:00Z';
+        return helpAsset;
     }
 
     /**
