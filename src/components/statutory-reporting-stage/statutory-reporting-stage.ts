be506a45c7d0ebf9c5531cc50ee25e4890b16eaa
T329354 <remazouari-ext@airfrance.fr>
2018-10-30 16:34:44 +0100

    EDOSPNC-405 Revue PR

--- src/components/statutory-reporting-stage/statutory-reporting-stage.ts
+++ src/components/statutory-reporting-stage/statutory-reporting-stage.ts
@@ -4,15 +4,10 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'statutory-reporting-stage',
   templateUrl: 'statutory-reporting-stage.html'
 })
-export class StatutoryReportingStageComponent implements OnInit {
+export class StatutoryReportingStageComponent {
+
+  expandedHeight = '48px';
 
   @Input() stagesList;
 
-  constructor() {
-
-  }
-
-  ngOnInit() {
-
-  }
 }
