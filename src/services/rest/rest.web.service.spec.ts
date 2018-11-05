2f6ce574c516d116a5231918479227def469c7fa
T778676 <T778676@airfrance.fr>
2018-11-05 10:25:19 +0100

    EDOSPNC-403 Mise an place tests

--- src/services/rest/rest.web.service.spec.ts
+++ src/services/rest/rest.web.service.spec.ts
@@ -2,19 +2,22 @@ import { TestBed, fakeAsync, flush } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RestWebService } from './rest.web.service';
 import { Config } from '../../configuration/environment-variables/config';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('Tests des appels rests en mode Web', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
+            imports: [
+                HttpClientModule
+            ],
             providers: [
+                HttpClientModule,
                 RestWebService,
                 Config
             ]
         });
     });
-
 
     describe('On vérifie que l\'ensemble des URL définies dans l\'appli sont bien défini dans le back', () => {
 
@@ -22,15 +25,22 @@ describe('Tests des appels rests en mode Web', () => {
         let config;
         beforeEach(() => {
             config = new Config();
+            config.backEndUrl = 'http://localhost:8080/api/rest/resources';
             URL = config.getBackEndUrl('getCareerObjectives');
         });
 
-        it('doit renvoyer une réponse lors de l\'appel au back', fakeAsync(() => {
+        it('doit renvoyer une réponse lors de l\'appel au back', () => {
             const restWebService = TestBed.get(RestWebService);
-            const http = TestBed.get(HttpTestingController);
+            // const http = TestBed.get(HttpTestingController);
 
-            const result = http.expectOne(`http://www.google.fr`, 'call to API');
-            http.verify();
-        }));
+            restWebService.get(URL).then(data => {
+                console.log('data', data);
+                expect(data.length).toBe(1);
+            });
+
+            /* const result = http.expectOne(URL, 'call to API');
+             console.log(result);
+             http.verify();*/
+        });
     });
 });
