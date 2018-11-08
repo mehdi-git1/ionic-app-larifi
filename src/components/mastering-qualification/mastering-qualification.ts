2cb27f51782d1db3f0d79baf3c18d454b21a0db7
T329354 <remazouari-ext@airfrance.fr>
2018-11-08 12:02:28 +0100

    EDOSPNC-429 Affichage des qualifications maitrises

--- src/components/mastering-qualification/mastering-qualification.ts
+++ src/components/mastering-qualification/mastering-qualification.ts
@@ -8,24 +8,36 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MasteringQualificationComponent implements OnInit {
 
-  @Input() masteringQualificationData: MasteringQualification;
+  @Input() masteringQualificationsData: Array<MasteringQualification>;
 
   // Tableau des valeurs à afficher en fonction du type de tableau
   masteringQualificationDisplayedData;
 
-  constructor(private translateService: TranslateService) {
+  // Objet temporaire pour formater les données à afficher
+  tempMasteringQualificationData;
 
+  constructor(private translateService: TranslateService) {
+    this.tempMasteringQualificationData = {
+      stages: new Array(),
+      seniorityDate: new Array()
+    };
   }
   ngOnInit() {
+    if (this.masteringQualificationsData && this.masteringQualificationsData.length > 0) {
+      for (const masteringQualificationData of this.masteringQualificationsData) {
+        this.tempMasteringQualificationData.stages.push(masteringQualificationData.stage);
+        this.tempMasteringQualificationData.seniorityDate.push(masteringQualificationData.seniorityDate);
+      }
+    }
 
     this.masteringQualificationDisplayedData = {
       headers: [
-        this.translateService.instant('STATUTORY_CERTIFICATE.MASTERING_QUALIFICATION.SPECIALITY'),
-        this.translateService.instant('STATUTORY_CERTIFICATE.MASTERING_QUALIFICATION.START_DATE')
-      ], values: this.masteringQualificationData ?
+        this.translateService.instant('STATUTORY_CERTIFICATE.MASTERING_QUALIFICATION.STAGE'),
+        this.translateService.instant('STATUTORY_CERTIFICATE.MASTERING_QUALIFICATION.SENIORITY_DATE')
+      ], values: this.masteringQualificationsData && this.masteringQualificationsData.length > 0 ?
         [
-          { value: [this.masteringQualificationData.currentSpeciality], type: 'text' },
-          { value: [this.masteringQualificationData.seniorityDate], type: 'date' }
+          { value: this.tempMasteringQualificationData.stages, type: 'text' },
+          { value: this.tempMasteringQualificationData.seniorityDate, type: 'date' }
         ]
         :
         null
