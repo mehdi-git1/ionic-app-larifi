c044e73c563c3f99f437180514d79a787757c693
DUMONT Thibaut <thdumont1@airfrance.fr>
2018-12-05 16:49:30 +0100

    EDOSPNC-545 Fix tabnav si imperso et user non pnc

--- src/app/shared/components/tab-nav/tab-nav.component.ts
+++ src/app/shared/components/tab-nav/tab-nav.component.ts
@@ -54,7 +54,7 @@ export class TabNavComponent {
     private specialityService: SpecialityService
   ) {
     this.events.subscribe('user:authenticationDone', () => {
-      if (this.sessionService.getActiveUser()) {
+      if (this.sessionService.getActiveUser() && this.sessionService.getActiveUser().isPnc) {
         this.pncProvider.getPnc(this.sessionService.getActiveUser().matricule).then(pnc => {
           this.pnc = pnc;
           this.pncParams = this.pnc;
