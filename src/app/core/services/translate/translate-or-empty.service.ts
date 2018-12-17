a56467c8539ad7dc8c277c8c60986e43ff8fd71b
Ludovic CHEMIR <luchemir-ext@airfrance.com>
2018-12-17 10:01:36 +0100

    EDOSPNC-230 Modif JSDoc

--- src/app/core/services/translate/translate-or-empty.service.ts
+++ src/app/core/services/translate/translate-or-empty.service.ts
@@ -9,9 +9,9 @@ export class TranslateOrEmptyService {
     }
 
     /**
-     * Traduit la clé en valeur à partir du fichier d'internationalisation. Renvoie vide si la clé est nulle, undefined ou si la clé et la valeur sont identiques
+     * Traduit la clé en valeur à partir du fichier d'internationalisation
      * @param key clé du message
-     * @return la valeur dans la fichier d'internationalisation
+     * @return la valeur dans la fichier d'internationalisation ou renvoie vide si la clé est nulle, undefined ou si la clé et la valeur sont identiques
      */
     public translate(key: string): string {
         if (!key || key === undefined) {
