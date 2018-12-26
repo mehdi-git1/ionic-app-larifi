7f8d5dbf57b845f3e12c8b10c2307f069a5ed650
T778676 <T778676@airfrance.fr>
2018-12-26 17:32:14 +0100

    EDOSPNC-595 tests correction

--- src/app/shared/components/offline-indicator/offline-indicator.component.ts
+++ src/app/shared/components/offline-indicator/offline-indicator.component.ts
@@ -1,32 +1,50 @@
-import { Component, Input, OnChanges } from '@angular/core';
-
-import * as moment from 'moment';
-
 import { TransformerService } from '../../../core/services/transformer/transformer.service';
+import { WaypointTransformerService } from '../../../core/services/waypoint/waypoint-transformer.service';
+import { CareerObjectiveTransformerService } from '../../../core/services/career-objective/career-objective-transformer.service';
 import { StorageService } from '../../../core/storage/storage.service';
 import { EntityEnum } from '../../../core/enums/entity.enum';
+import { WaypointService } from '../../../core/services/waypoint/waypoint.service';
+import { CareerObjectiveService } from '../../../core/services/career-objective/career-objective.service';
+import { LegService } from '../../../core/services/leg/leg.service';
+import { RotationService } from '../../../core/services/rotation/rotation.service';
+import { WaypointModel } from '../../../core/models/waypoint.model';
+import { CareerObjectiveModel } from '../../../core/models/career-objective.model';
+import { LegModel } from '../../../core/models/leg.model';
+import { PncTransformerService } from '../../../core/services/pnc/pnc-transformer.service';
 import { OfflineService } from '../../../core/services/offline/offline.service';
+import { PncService } from '../../../core/services/pnc/pnc.service';
+import { PncModel } from '../../../core/models/pnc.model';
 import { AppConstant } from '../../../app.constant';
+import { Component, Input, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 import { EDossierPncObjectModel } from '../../../core/models/e-dossier-pnc-object.model';
+import * as moment from 'moment';
+import { RotationModel } from '../../../core/models/rotation.model';
+
 
 
 @Component({
   selector: 'offline-indicator',
   templateUrl: 'offline-indicator.component.html'
 })
-export class OfflineIndicatorComponent implements OnChanges {
+export class OfflineIndicatorComponent {
 
-
-  @Input() private type: EntityEnum;
-
-  @Input() private offlineDataToCheck: EDossierPncObjectModel;
+  _object: any;
 
   constructor(private offlineProvider: OfflineService,
     private storageService: StorageService,
     private transformerService: TransformerService) {
   }
 
-  ngOnChanges() {
+  @Input()
+  private type: EntityEnum;
+
+  get object(): any {
+    return this._object;
+  }
+
+  @Input()
+  set object(val: any) {
+    this._object = val;
     this.refreshOffLineDateOnCurrentObject();
   }
 
@@ -36,12 +54,12 @@ export class OfflineIndicatorComponent implements OnChanges {
    */
   getCssClass(): string {
     // Si aucune date de stockage offline, c'est que l'objet n'est pas en cache
-    if (!this.offlineDataToCheck.offlineStorageDate) {
+    if (!this._object.offlineStorageDate) {
       return '';
     }
 
     const now = moment();
-    const offlineStorageDate = moment(this.offlineDataToCheck.offlineStorageDate, AppConstant.isoDateFormat);
+    const offlineStorageDate = moment(this._object.offlineStorageDate, AppConstant.isoDateFormat);
     const offlineDuration = moment.duration(now.diff(offlineStorageDate)).asMilliseconds();
 
     const upToDateThreshold = moment.duration(1, 'days').asMilliseconds();
@@ -61,20 +79,19 @@ export class OfflineIndicatorComponent implements OnChanges {
    * @return vrai si l'objet est en attente de synchro, faux sinon
    */
   hasBeenModifiedOffline(): boolean {
-    return this.offlineDataToCheck.offlineAction !== undefined && this.offlineDataToCheck.offlineAction !== null;
+    return this.object.offlineAction !== undefined && this.object.offlineAction !== null;
   }
-
   /**
    * Appelle la méthode refresh du provider de l'entité correspondante afin de mettre a jour la date de l'objet en cache sur l'objet online
    */
   refreshOffLineDateOnCurrentObject(): void {
-    const transformedObject: EDossierPncObjectModel = this.transformerService.transformObject(this.type, this.offlineDataToCheck);
+    const transformedObject: EDossierPncObjectModel = this.transformerService.transformObject(this.type, this._object);
 
     if (transformedObject) {
       this.storageService.findOneAsync(this.type, transformedObject.getStorageId()).then(offlineObject => {
         const offlineData = this.transformerService.transformObject(this.type, offlineObject);
         this.offlineProvider.flagDataAvailableOffline(transformedObject, offlineData);
-        this.offlineDataToCheck = transformedObject;
+        this._object = transformedObject;
       });
     }
 
