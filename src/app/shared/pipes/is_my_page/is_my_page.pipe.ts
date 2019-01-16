4cec5e4881683b56ff8d8a3b1ddd0ec918825ad3
DUMONT Thibaut <thdumont1@airfrance.fr>
2019-01-16 15:58:34 +0100

    EDOSPNC-632 Refacto isMyPipe

--- src/app/shared/pipes/is_my_page/is_my_page.pipe.ts
+++ src/app/shared/pipes/is_my_page/is_my_page.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { SessionService } from '../../../core/services/session/session.service';
+import { PncModel } from '../../../core/models/pnc.model';
 
 @Pipe({
     name: 'isMyPage'
@@ -11,26 +12,29 @@ export class IsMyPage implements PipeTransform {
     ) { }
 
     /**
-     * Récupére le titre personnel si on est sur une de nos pages
+     * Récupère le titre personnel si on est sur une de nos pages
      * Sinon, si on est sur une page d'un PNC, on met le titre normal
-     * @param value : Valeur du champ à traduire
+     * @param localizationKey une clef de localization
+     * @return la nouvelle valeur
      */
-    transform(value, pnc) {
+    transform(localizationKey: string, pnc: PncModel): string {
         // La condition pnc === undefined est nécessaire pour lorsque l'on vient du header
         // On considére que si il n'y a pas de pnc en entré, c'est que l'on est sur une page de l'utilisateur principal
         if (this.sessionService.isActiveUser(pnc) || pnc === undefined) {
-            return this.addMyToTranslate(value);
+            return this.addMyToTranslate(localizationKey);
         }
-        return value;
+        return localizationKey;
     }
 
     /**
-     * Transforme le libellé en libellé personnel
+     * Ajoute à la clef de localisation un préfixe "MY_" permettant de pointer vers son pendant possessif
+     * @param localizationKey une clef de localisation
+     * @return la nouvelle clef de localisation
      */
-    addMyToTranslate(value) {
-        const tmpValue = value.split('.');
-        tmpValue[tmpValue.length - 1] = 'MY_' + tmpValue[tmpValue.length - 1];
-        return tmpValue.join('.');
+    addMyToTranslate(localizationKey: string): string {
+        const splitLocalizationKey = localizationKey.split('.');
+        splitLocalizationKey[splitLocalizationKey.length - 1] = 'MY_' + splitLocalizationKey[splitLocalizationKey.length - 1];
+        return splitLocalizationKey.join('.');
     }
 
 }
