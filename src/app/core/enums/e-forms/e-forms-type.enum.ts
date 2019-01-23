54580e848b3eb9f5cbd58d73765828f547ee575e
T778676 <T778676@airfrance.fr>
2019-01-23 17:09:45 +0100

    EDOSPNC-490 Redesign du bouton création eObs

---
+++ src/app/core/enums/e-forms/e-forms-type.enum.ts
@@ -0,0 +1,40 @@
+export enum EFormsTypeEnum {
+    'HOT',
+    'STW',
+    'CC',
+    'CCP',
+    'ALT',
+    'PCB'
+}
+
+export namespace EFormsTypeEnum {
+    /**
+     * retourne le bon Type en fonction de l'enum
+     * @param type enum à tester
+     */
+    export function getType(type: EFormsTypeEnum) {
+        switch (type) {
+            case EFormsTypeEnum['HOT']: return '16';
+            case EFormsTypeEnum['STW']: return '16';
+            case EFormsTypeEnum['CC']: return '17';
+            case EFormsTypeEnum['CCP']: return '18';
+            default: return null;
+        }
+    }
+
+    /**
+     * retourne le bon Text de tyê de formulaire en fonction de l'enum
+     * @param type enum à tester
+     */
+    export function getTextType(type: EFormsTypeEnum) {
+        switch (type) {
+            case EFormsTypeEnum['HOT']: return 'eHST';
+            case EFormsTypeEnum['STW']: return 'eHST';
+            case EFormsTypeEnum['CC']: return 'eCC';
+            case EFormsTypeEnum['CCP']: return 'eCCP';
+            case EFormsTypeEnum['ALT']: return 'eALT';
+            case EFormsTypeEnum['PCB']: return 'ePCB';
+            default: return null;
+        }
+    }
+}
