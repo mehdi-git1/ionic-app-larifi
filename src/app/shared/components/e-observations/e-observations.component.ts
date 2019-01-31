1ad9d4347359ba278ba130bb57d72064bec2683d
T778676 <T778676@airfrance.fr>
2019-01-31 10:14:20 +0100

    EDOSPNC-388 Finition de l'US (reste Tests à faire)

--- src/app/shared/components/e-observations/e-observations.component.ts
+++ src/app/shared/components/e-observations/e-observations.component.ts
@@ -12,6 +12,7 @@ export class EObservationsComponent implements OnChanges {
 
   matPanelHeaderHeight = 'auto';
   isOlderThan3Years = false;
+  nbOfYearsToChangeMessage = 3;
 
   @Input() eObservations: EObservationModel[];
 
@@ -21,9 +22,13 @@ export class EObservationsComponent implements OnChanges {
   ngOnChanges() {
     if (this.eObservations[0]) {
       this.isOlderThan3Years = moment.duration(
-        moment(this.eObservations[0].rotationDate)
+        moment(moment())
           .diff(moment(this.eObservations[this.eObservations.length - 1].rotationDate)))
-        .asYears() > 3;
+        .asYears() > this.nbOfYearsToChangeMessage;
+      console.log(moment.duration(
+        moment(moment())
+          .diff(moment(this.eObservations[this.eObservations.length - 1].rotationDate)))
+        .asYears());
     }
   }
 
