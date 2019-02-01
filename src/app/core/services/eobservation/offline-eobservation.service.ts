197a1cf7772fdb8d198d645716acac82e8d71fa2
T778676 <T778676@airfrance.fr>
2019-02-01 16:03:30 +0100

    EDOSPNC-388 Retour de PR

--- src/app/core/services/eobservation/offline-eobservation.service.ts
+++ src/app/core/services/eobservation/offline-eobservation.service.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@angular/core';
 import { EntityEnum } from '../../enums/entity.enum';
 import { StorageService } from '../../storage/storage.service';
-import { SessionService } from '../session/session.service';
 import { EObservationModel } from '../../models/eobservation.model';
 
 @Injectable()
 export class OfflineEObservationService {
 
     constructor(
-        private storageService: StorageService,
-        private sessionService: SessionService
+        private storageService: StorageService
     ) {
     }
 
     /**
-     * Sauvegarde un PNC dans le cache
-     * @param pnc le PNC à sauvegarder
-     * @return une promesse contenant le PNC sauvé
+     * Sauvegarde une eObservation dans le cache
+     * @param eObservation l'eObservation à sauvegarder
+     * @return une promesse contenant l'eObservation sauvé
      */
-    save(pnc: EObservationModel): Promise<EObservationModel[]> {
-        return this.storageService.saveAsync(EntityEnum.EOBSERVATION, pnc);
+    save(eObservation: EObservationModel): Promise<EObservationModel[]> {
+        return this.storageService.saveAsync(EntityEnum.EOBSERVATION, eObservation);
     }
 
     /**
