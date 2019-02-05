57c8645f34ab908f4cac16a721d84795970e9211
DUMONT Thibaut <thdumont1@airfrance.fr>
2019-02-05 14:54:57 +0100

    EDOSPNC-640 Tri des lettres offline

--- src/app/core/services/congratulation-letter/offline-congratulation-letter.service.ts
+++ src/app/core/services/congratulation-letter/offline-congratulation-letter.service.ts
@@ -1,9 +1,9 @@
+import { AppConstant } from './../../../app.constant';
 import { CongratulationLetterModel } from './../../models/congratulation-letter.model';
-import { OfflineActionEnum } from '../../enums/offline-action.enum';
-import { CareerObjectiveModel } from '../../models/career-objective.model';
 import { Injectable } from '@angular/core';
 import { StorageService } from '../../storage/storage.service';
 import { EntityEnum } from '../../enums/entity.enum';
+import * as moment from 'moment';
 
 
 @Injectable()
@@ -20,12 +20,16 @@ export class OfflineCongratulationLetterService {
   getReceivedCongratulationLetters(pncMatricule: string): Promise<CongratulationLetterModel[]> {
     return new Promise((resolve) => {
       const congratulationLetterList = this.storageService.findAll(EntityEnum.CONGRATULATION_LETTER);
-      const receivedCongratulationLetters = congratulationLetterList.filter(congratulationLetter => {
+      let receivedCongratulationLetters = congratulationLetterList.filter(congratulationLetter => {
         // On filtre les PNC concernés pour voir si le PNC recherché est un destinataire
         const concernedPncs = congratulationLetter.concernedPncs.filter(pnc => {
           return pnc.matricule === pncMatricule;
         });
         return concernedPncs.length > 0;
+      });
+      // On tri par date de création décroissante
+      receivedCongratulationLetters = receivedCongratulationLetters.sort((letter1, letter2) => {
+        return moment(letter1.creationDate, AppConstant.isoDateFormat).isAfter(moment(letter2.creationDate, AppConstant.isoDateFormat)) ? -1 : 1;
       });
       resolve(receivedCongratulationLetters);
     });
@@ -39,8 +43,12 @@ export class OfflineCongratulationLetterService {
   getWrittenCongratulationLetters(pncMatricule: string): Promise<CongratulationLetterModel[]> {
     return new Promise((resolve) => {
       const congratulationLetterList = this.storageService.findAll(EntityEnum.CONGRATULATION_LETTER);
-      const writtenCongratulationLetters = congratulationLetterList.filter(congratulationLetter => {
+      let writtenCongratulationLetters = congratulationLetterList.filter(congratulationLetter => {
         return congratulationLetter.redactor.matricule === pncMatricule;
+      });
+      // On tri par date de création décroissante
+      writtenCongratulationLetters = writtenCongratulationLetters.sort((letter1, letter2) => {
+        return moment(letter1.creationDate, AppConstant.isoDateFormat).isAfter(moment(letter2.creationDate, AppConstant.isoDateFormat)) ? -1 : 1;
       });
       resolve(writtenCongratulationLetters);
     });
