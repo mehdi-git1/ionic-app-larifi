018bd6209743837198641aa4a6956f5c1d6132e9
DUMONT Thibaut <thdumont1@airfrance.fr>
2019-02-06 14:51:38 +0100

    EDOSPNC-640 ajout JSDoc sur transformer

--- src/app/core/services/congratulation-letter/congratulation-letter-transformer.service.ts
+++ src/app/core/services/congratulation-letter/congratulation-letter-transformer.service.ts
@@ -9,6 +9,11 @@ export class CongratulationLetterTransformerService {
   constructor() {
   }
 
+  /**
+  * Transforme un tableau d'objets en tableau de CongratulationLetterModel
+  * @param object le tableau d'objet à transformer
+  * @return un tableau d'objets de type CongratulationLetterModel
+  */
   toCongratulationLetters(array: CongratulationLetterModel[]) {
     const newArray: CongratulationLetterModel[] = [];
     for (const object of array) {
@@ -17,6 +22,11 @@ export class CongratulationLetterTransformerService {
     return newArray;
   }
 
+  /**
+   * Transforme un objet en CongratulationLetterModel
+   * @param object l'objet à transformer
+   * @return un objet de type CongratulationLetterModel
+   */
   toCongratulationLetter(object: CongratulationLetterModel): CongratulationLetterModel {
     return !object ?
       object :
