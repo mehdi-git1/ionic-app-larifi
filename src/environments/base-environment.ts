a4797193220547ff81c2785a72e451edae9fe7f0
DUMONT Thibaut <thdumont1@airfrance.fr>
2019-02-26 13:33:11 +0100

    Déclaration version 1.5.1

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '1.5.0';
+  public appVersion = '1.5.1';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public backEndUrl;
