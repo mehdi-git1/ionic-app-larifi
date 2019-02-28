e7eec4d2d8de4a15066b6ce955e1345bedca60fa
T329354 <remazouari-ext@airfrance.fr>
2019-02-28 16:36:56 +0100

    EDOSPNC-540 Prévoir l'affichage d'autre aptitudes médical

--- src/app/modules/statutory-certificate/components/medical-aptitudes/medical-aptitudes.component.ts
+++ src/app/modules/statutory-certificate/components/medical-aptitudes/medical-aptitudes.component.ts
@@ -12,25 +12,27 @@ export class MedicalAptitudesComponent implements OnInit {
   @Input() medicalAptitudesData: MedicalAptitudesModel;
 
   // Tableau des valeurs à afficher en fonction du type de tableau
-  skillDisplayedData;
+  medicalAptitudesDisplayedData;
 
   constructor(private translateService: TranslateService) {
   }
 
   ngOnInit() {
-    this.skillDisplayedData = {
-      headers:
-        [
-          this.translateService.instant('STATUTORY_CERTIFICATE.MEDICAL_APTITUDES.START_DATE'),
-          this.translateService.instant('STATUTORY_CERTIFICATE.MEDICAL_APTITUDES.END_DATE')
-        ],
-      values: this.medicalAptitudesData ?
-        [
-          { value: [_.get(this.medicalAptitudesData, 'validityStartDate')], type: 'date' },
-          { value: [_.get(this.medicalAptitudesData, 'validityEndDate')], type: 'end-date' }
-        ]
-        :
-        null
+    this.medicalAptitudesDisplayedData = {
+      medicalVisit: {
+        headers:
+          [
+            this.translateService.instant('STATUTORY_CERTIFICATE.MEDICAL_APTITUDES.MEDICAL_VISIT.START_DATE'),
+            this.translateService.instant('STATUTORY_CERTIFICATE.MEDICAL_APTITUDES.MEDICAL_VISIT.END_DATE')
+          ],
+        values: this.medicalAptitudesData && this.medicalAptitudesData.medicalVisit ?
+          [
+            { value: [_.get(this.medicalAptitudesData, 'medicalVisit.validityStartDate')], type: 'date' },
+            { value: [_.get(this.medicalAptitudesData, 'medicalVisit.validityEndDate')], type: 'end-date' }
+          ]
+          :
+          null
+      }
     };
   }
 }
