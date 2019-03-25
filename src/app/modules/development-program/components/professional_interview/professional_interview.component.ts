88885616047b3512d7f9953276d4022cc7f8d0d6
T778676 <T778676@airfrance.fr>
2019-03-25 16:36:08 +0100

    EDSOCPNC-594 Affichage page en cours

--- src/app/modules/development-program/components/professional_interview/professional_interview.component.ts
+++ src/app/modules/development-program/components/professional_interview/professional_interview.component.ts
@@ -1,22 +1,32 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 import { ProfessionalInterviewModel } from '../../../../core/models/professional-interview/professional-interview.model';
+import { ProfessionalInterviewDetailsPage } from '../../../professional-interview/pages/professional-interview-details/professional-interview-details.page';
+
 
 @Component({
   selector: 'professional-interview',
   templateUrl: 'professional_interview.component.html'
 })
 
-export class ProfessionalInterviewComponent implements OnChanges {
+export class ProfessionalInterviewComponent {
 
   matPanelHeaderHeight = 'auto';
 
   @Input() professionalInterview: ProfessionalInterviewModel;
 
-  constructor() {
+  constructor(
+    private navCtrl: NavController
+  ) {
   }
 
-  ngOnChanges() {
-
+  /**
+    * Redirige vers le détail d'une eObservation
+    * @param evt event
+    */
+  goToProfessionalInterviewDetail(evt: Event): void {
+    evt.stopPropagation();
+    this.navCtrl.push(ProfessionalInterviewDetailsPage, { professionalInterview: this.professionalInterview });
   }
 }
--- src/app/modules/professional-interview/pages/professional-interview-details/professional-interview-details.page.ts
+++ src/app/modules/professional-interview/pages/professional-interview-details/professional-interview-details.page.ts
@@ -1,3 +1,4 @@
+import { ProfessionalInterviewModel } from './../../../../core/models/professional-interview/professional-interview.model';
 import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { EObservationModel } from '../../../../core/models/eobservation/eobservation.model';
@@ -24,26 +25,24 @@ import { Utils } from '../../../../shared/utils/utils';
 export class ProfessionalInterviewDetailsPage {
   PncRoleEnum = PncRoleEnum;
 
-  eObservation: EObservationModel;
-  originEObservation: EObservationModel;
-
-  itemsSortedByTheme: EobservationItemsByTheme[];
+  professionalInterview: ProfessionalInterviewModel;
+  originProfessionalInterview: ProfessionalInterviewModel;
 
   loading: Loading;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public navParams: NavParams,
     private eObservationService: EObservationService,
     private sessionService: SessionService,
     private translateService: TranslateService,
     private toastService: ToastService,
     private alertCtrl: AlertController,
-    private loadingCtrl: LoadingController) {
-    if (this.navParams.get('eObservation')) {
-      this.eObservation = this.navParams.get('eObservation');
-      this.originEObservation = _.cloneDeep(this.eObservation);
-
-      this.itemsSortedByTheme = this.sortEObservationItemsByTheme();
+    private loadingCtrl: LoadingController
+  ) {
+    if (this.navParams.get('professionalInterview')) {
+      this.professionalInterview = this.navParams.get('eObservation');
+      this.originProfessionalInterview = _.cloneDeep(this.professionalInterview);
     }
   }
 
@@ -59,7 +58,7 @@ export class ProfessionalInterviewDetailsPage {
   * Vérifie si le formulaire a été modifié sans être enregistré
   */
   formHasBeenModified() {
-    return Utils.getHashCode(this.originEObservation) !== Utils.getHashCode(this.eObservation);
+    return Utils.getHashCode(this.originProfessionalInterview) !== Utils.getHashCode(this.professionalInterview);
   }
 
   /**
@@ -95,61 +94,6 @@ export class ProfessionalInterviewDetailsPage {
     return crewMember;
   }
 
-  /**
-   * Trie les items d'eobs par theme
-   * @return la liste de EobservationItemsByTheme
-   */
-  sortEObservationItemsByTheme(): EobservationItemsByTheme[] {
-    const itemsByTheme = new Array<EobservationItemsByTheme>();
-    if (this.eObservation && this.eObservation.eobservationItems && this.eObservation.eobservationItems.length > 0) {
-      for (const eObservationItem of this.eObservation.eobservationItems.sort((a, b) => a.itemOrder > b.itemOrder ? 1 : -1)) {
-        const eObservationTheme = eObservationItem.refItemLevel.item.theme;
-        let themeToDisplay = itemsByTheme.find(element => eObservationTheme.label == element.referentialTheme.label);
-        if (!themeToDisplay) {
-          themeToDisplay = new EobservationItemsByTheme(eObservationTheme);
-          itemsByTheme.push(themeToDisplay);
-        }
-        themeToDisplay.eObservationItems.push(eObservationItem);
-      }
-    }
-    return itemsByTheme.sort((a, b) => a.referentialTheme.themeOrder > b.referentialTheme.themeOrder ? 1 : -1);
-  }
-
-  /**
-   * Récupère les itemLevels referentiel de le l'item referentiel
-   * @return la liste des ReferentialItemLevelModel
-   */
-  getRefItemLevelsByRefItem(): ReferentialItemLevelModel[] {
-    const refItemLevels = new Array<ReferentialItemLevelModel>();
-    let refItemLevel = new ReferentialItemLevelModel();
-    refItemLevel.level = EObservationLevelEnum.LEVEL_1;
-    refItemLevels.push(refItemLevel);
-    refItemLevel = new ReferentialItemLevelModel();
-    refItemLevel.level = EObservationLevelEnum.LEVEL_2;
-    refItemLevels.push(refItemLevel);
-    refItemLevel = new ReferentialItemLevelModel();
-    refItemLevel.level = EObservationLevelEnum.LEVEL_3;
-    refItemLevels.push(refItemLevel);
-    refItemLevel = new ReferentialItemLevelModel();
-    refItemLevel.level = EObservationLevelEnum.LEVEL_4;
-    refItemLevels.push(refItemLevel);
-    refItemLevel = new ReferentialItemLevelModel();
-    refItemLevel.level = EObservationLevelEnum.NO;
-    refItemLevels.push(refItemLevel);
-    return refItemLevels;
-  }
-
-  /**
-   * Vérifie qu'il y a des vols
-   *
-   * @return true si il n'y a pas de vols dans cette eobs, sinon false
-   */
-  hasFlights(): boolean {
-    if (!this.eObservation || !this.eObservation.eobservationFlights || this.eObservation.eobservationFlights.length === 0) {
-      return false;
-    }
-    return true;
-  }
 
   /**
    * Définit la couleur en fonction du statut
@@ -157,9 +101,9 @@ export class ProfessionalInterviewDetailsPage {
    * @return 'green' si 'TAKEN_INTO_ACCOUNT' ou 'red' si 'NOT_TAKEN_INTO_ACCOUNT'
    */
   getColorStatusPoint(): string {
-    if (this.eObservation && this.eObservation.state === EObservationStateEnum.TAKEN_INTO_ACCOUNT) {
+    if (this.professionalInterview && this.professionalInterview.state === EObservationStateEnum.TAKEN_INTO_ACCOUNT) {
       return 'green';
-    } else if (this.eObservation && this.eObservation.state === EObservationStateEnum.NOT_TAKEN_INTO_ACCOUNT) {
+    } else if (this.professionalInterview && this.professionalInterview.state === EObservationStateEnum.NOT_TAKEN_INTO_ACCOUNT) {
       return 'red';
     }
   }
@@ -169,10 +113,10 @@ export class ProfessionalInterviewDetailsPage {
    * @return le label à afficher
    */
   getTypeLabel(): string {
-    if (!this.eObservation) {
+    if (!this.professionalInterview) {
       return '';
     }
-    return EObservationTypeEnum.getLabel(this.eObservation.type);
+    return EObservationTypeEnum.getLabel(this.professionalInterview.type);
   }
 
   /**
@@ -180,38 +124,19 @@ export class ProfessionalInterviewDetailsPage {
    * @return le label à afficher
    */
   getDetailOptionType(): string {
-    return this.eObservationService.getDetailOptionType(this.eObservation);
+    return this.eObservationService.getDetailOptionType(this.professionalInterview);
   }
 
-  /**
-   * Définit si la periode temporaire est affichable
-   * @return true si 'ECC' ou 'ECCP' et si l'une des valeurs "vol de formation" ou "val" est true
-   */
-  hasTemporaryPeriodToBeDisplayed(): boolean {
-    return this.eObservation
-      && (this.eObservation.type === EObservationTypeEnum.E_CC || this.eObservation.type === EObservationTypeEnum.E_CCP)
-      && (this.eObservation.formationFlight || this.eObservation.val);
-  }
-
-  /**
-   * Trie les vols de l'eobs
-   */
-  sortedFlights(): EObservationFlightModel[] {
-    if (this.eObservation && this.eObservation.eobservationFlights) {
-      return this.eObservation.eobservationFlights.sort((a, b) => a.flightOrder > b.flightOrder ? 1 : -1);
-    }
-    return new Array();
-  }
 
   /**
    * Teste si le commentaire PNC peut être modifié
    * @return vrai si le commentaire peut être modifié, faux sinon
    */
   canEditPncComment(): boolean {
-    return this.sessionService.getActiveUser().matricule === this.eObservation.pnc.matricule
-      && (this.originEObservation.pncComment === '' || typeof (this.originEObservation.pncComment) === 'undefined')
-      && this.eObservation.type !== EObservationTypeEnum.E_ALT
-      && this.eObservation.type !== EObservationTypeEnum.E_PCB;
+    return this.sessionService.getActiveUser().matricule === this.professionalInterview.pnc.matricule
+      && (this.professionalInterview.pncComment === '' || typeof (this.originProfessionalInterview.pncComment) === 'undefined')
+      && this.professionalInterview.type !== EObservationTypeEnum.E_ALT
+      && this.professionalInterview.type !== EObservationTypeEnum.E_PCB;
   }
 
   /**
@@ -241,10 +166,10 @@ export class ProfessionalInterviewDetailsPage {
     this.loading = this.loadingCtrl.create();
     this.loading.present();
     // On transmet un objet cloné pour éviter toute modif de l'objet par le service
-    const eObservationClone = _.cloneDeep(this.eObservation);
+    const eObservationClone = _.cloneDeep(this.professionalInterview);
     this.eObservationService.validatePncComment(eObservationClone).then(eObservation => {
-      this.eObservation = eObservation;
-      this.originEObservation = _.cloneDeep(this.eObservation);
+      this.professionalInterview = eObservation;
+      this.originEObservation = _.cloneDeep(this.professionalInterview);
       this.toastService.success(this.translateService.instant('EOBSERVATION.MESSAGES.SUCCESS.PNC_COMMENT_SAVED'));
       this.navCtrl.pop();
     }, error => { }).then(() => {
@@ -258,6 +183,6 @@ export class ProfessionalInterviewDetailsPage {
    * @return true si c'est le cas, false sinon
    */
   loadingIsOver(): boolean {
-    return this.eObservation !== undefined;
+    return this.professionalInterview !== undefined;
   }
 }
