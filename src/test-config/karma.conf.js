173371b19086065775974c8b5d08d4ecc85c09d6
T778676 <T778676@airfrance.fr>
2019-04-10 15:21:19 +0200

    EDOSPNC-594 finitions

--- src/test-config/karma.conf.js
+++ src/test-config/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
         angularCli: {
             environment: 'dev'
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml', 'junit'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
