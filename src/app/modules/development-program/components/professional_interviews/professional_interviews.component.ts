0ccaa270dd85e512b81af859eab49ddbbf1dbc50
DUMONT THIBAUT <AM414147@ad.airfrance.fr>
2019-04-15 15:28:29 +0200

    EDOSPNC-593 Retour de tests

--- src/app/modules/development-program/components/professional_interviews/professional_interviews.component.ts
+++ src/app/modules/development-program/components/professional_interviews/professional_interviews.component.ts
@@ -11,8 +11,14 @@ import { ProfessionalInterviewModel } from '../../../../core/models/professional
 export class ProfessionalInterviewsComponent {
 
   matPanelHeaderHeight = '41px';
+  professionalInterviewTab: ProfessionalInterviewModel[];
 
-  @Input() professionalInterviews: ProfessionalInterviewModel[];
   @Input() legend = true;
+  @Input()
+  set professionalInterviews(val: any) {
+    this.professionalInterviewTab = val.sort((pi: ProfessionalInterviewModel, otherPi: ProfessionalInterviewModel) => {
+        return pi.annualProfessionalInterviewDate < otherPi.annualProfessionalInterviewDate ? 1 : -1;
+      });
+    }
 
 }
