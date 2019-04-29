2f7d3ffbaf382b02d6c7cbba61bee33f0ed0d1c2
M429676 <paramane@airfrance.fr>
2019-04-29 16:55:31 +0200

    EDOSPNC-886 sidebar pour accéder aux différentes fonctionnalités admin

--- src/app/modules/admin/pages/admin-home/admin-home.page.ts
+++ src/app/modules/admin/pages/admin-home/admin-home.page.ts
@@ -1,34 +1,63 @@
+import { MenuController, Nav } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
 import { UserMessageManagementPage } from './../user-message-management/user-message-management.page';
 import { AppVersionManagementPage } from './../app-version-management/app-version-management.page';
 import { ProfileManagementPage } from './../profile-management/profile-management.page';
-import { NavController } from 'ionic-angular';
-import { Component } from '@angular/core';
+import { PncHomePage } from 'E:/git/edospnc-cs/src/app/modules/home/pages/pnc-home/pnc-home.page';
 
 @Component({
     selector: 'admin-home',
     templateUrl: 'admin-home.page.html',
 })
 export class AdminHomePage {
-    constructor(private navCtrl: NavController) { }
 
-    /**
-     * Redirige vers la page de gestion des profils
-     */
-    goToProfileManagement() {
-        this.navCtrl.push(ProfileManagementPage);
+    @ViewChild(Nav) nav: Nav;
+
+    rootPage: any = ProfileManagementPage;
+    activePage: any;
+
+    pages: Array<{ title: string, component: any }>;
+
+    constructor(public menuCtrl: MenuController,
+        private translateService: TranslateService
+    ) {
+        this.pages = [
+            { title: this.translateService.instant('ADMIN.ADMIN_HOME.BUTTON.PROFILE_MANAGEMENT'), component: ProfileManagementPage },
+            { title: this.translateService.instant('ADMIN.ADMIN_HOME.BUTTON.APP_VERSION_MANAGEMENT'), component: AppVersionManagementPage },
+            { title: this.translateService.instant('ADMIN.ADMIN_HOME.BUTTON.USER_MESSAGE_MANAGEMENT'), component: UserMessageManagementPage }
+        ];
+    }
+
+    ionViewWillEnter() {
+        this.activePage = this.pages[0];
     }
 
     /**
-     * Redirige vers la page de gestion des versions
+     * Ouvre le menu d'administration
      */
-    goToAppVersionManagement() {
-        this.navCtrl.push(AppVersionManagementPage);
+    openMenu() {
+        this.menuCtrl.open();
+    }
+    /**
+     * Permet d'ouvrir une page à travers le menu
+     * @param page la page à ouvrir
+     */
+    openPage(page) {
+        this.nav.setRoot(page.component);
+        // Le menu se ferme lors de la sélection d'une page
+        this.menuCtrl.close();
+        this.activePage = page;
     }
 
     /**
-     * Redirige vers la page de gestion des messages utilisateur
+     * permet de savoir si une page est active ou non
+     * @param page activé
      */
-    goToUserMessageManagement() {
-        this.navCtrl.push(UserMessageManagementPage);
+    checkActive(page) {
+        return page == this.activePage;
     }
+
+
 }
