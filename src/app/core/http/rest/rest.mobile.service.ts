0459227f7b3f515ae9b172e90fb10ef58ecdd699
Mazouari <T329354@airfrance.fr>
2019-05-09 15:47:42 +0200

    EDOSPNC-912 Suppression du content-type du header coté mobile

--- src/app/core/http/rest/rest.mobile.service.ts
+++ src/app/core/http/rest/rest.mobile.service.ts
@@ -37,7 +37,6 @@ export class RestMobileService extends RestService {
 
         if ((request.method == 'POST' || request.method == 'PUT') && request.jsonData) {
             request.httpHeaders = Object.assign(request.httpHeaders, {
-                'Content-Type': 'application/json',
                 'Accept': '*/*'
             });
 
