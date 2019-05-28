d3d23c0b91e01a7d847a60cfe9b3efc5f090f272
iMac Paray <>
2019-05-28 15:38:30 +0200

    Mise a jour du numero de version 1.8.0 to 1.8.1

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '1.8.0';
+  public appVersion = '1.8.1';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public backEndUrl;
