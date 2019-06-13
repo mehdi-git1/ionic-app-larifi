9e35ddac8a59237004150d665370a8bc328af1b2
DUMONT Thibaut <thdumont1@airfrance.fr>
2019-06-13 15:32:53 +0200

    EDOSPNC-885 Ajout TU tab nav

---
+++ src/app/core/services/tab-nav/tab-nav.service.spec.ts
@@ -0,0 +1,25 @@
+import { TabNavEnum } from '../../enums/tab-nav.enum';
+import { TabNavService } from './tab-nav.service';
+
+describe('tabNavService', () => {
+
+    let tabNavService: TabNavService;
+
+    beforeEach(() => {
+        tabNavService = new TabNavService();
+        tabNavService.listOfTab = [
+            { id: TabNavEnum.PNC_HOME_PAGE },
+            { id: TabNavEnum.PNC_SEARCH_PAGE },
+            { id: TabNavEnum.UPCOMING_FLIGHT_LIST_PAGE },
+            { id: TabNavEnum.HELP_ASSET_LIST_PAGE }
+        ];
+    });
+
+    describe('findTabIndex', () => {
+        it(`doit ramener  lorsque le paramêtre est ${TabNavEnum.PNC_SEARCH_PAGE} `, () => {
+            const indexReturn = tabNavService.getTabIndex(TabNavEnum.PNC_SEARCH_PAGE);
+            expect(indexReturn).toBe(1);
+        });
+    });
+
+});
