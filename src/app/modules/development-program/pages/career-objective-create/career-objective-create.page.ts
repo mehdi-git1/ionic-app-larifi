2ae9c5427ae81b225ff5da06dded1bc179d17054
Gustave Dit Duflo <M409346@airfrance.fr>
2019-06-18 14:52:14 +0200

    EDOSPNC-925 - Notification instructeur avec mode déconnecté

--- src/app/modules/development-program/pages/career-objective-create/career-objective-create.page.ts
+++ src/app/modules/development-program/pages/career-objective-create/career-objective-create.page.ts
@@ -460,7 +460,7 @@ export class CareerObjectiveCreatePage {
     }
 
     /**
-    * La méthode sauvegarde le brouillon, tout en envoyant une notification à l'instructeur
+    * Sauvegarde le brouillon, tout en envoyant une notification à l'instructeur
     */
     saveCareerObjectiveDraftWithNotification() {
         this.careerObjective.instructorToBeNotified = true;
