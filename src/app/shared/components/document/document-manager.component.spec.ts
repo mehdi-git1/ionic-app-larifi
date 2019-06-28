bcdc0c1a030dd7f39bade5e5546d686e68bc2368
Ludovic CHEMIR <luchemir-ext@airfrance.com>
2019-06-28 16:26:24 +0200

    EDOSPNC-858 Clean code

--- src/app/shared/components/document/document-manager.component.spec.ts
+++ src/app/shared/components/document/document-manager.component.spec.ts
@@ -28,22 +28,22 @@ describe('documentManager', () => {
 
     describe('getFileTypeFromFile', () => {
       it(`doit ramener IMAGE si le fichier est de type image`, () => {
-        let arrayOfBlob = new Array<Blob>();
-        let file = new File( arrayOfBlob, 'test-file.jpg', { type: 'image/jpeg'});
+        const arrayOfBlob = new Array<Blob>();
+        const file = new File( arrayOfBlob, 'test-file.jpg', { type: 'image/jpeg'});
         const fileType = documentManagerComponent.getFileTypeFromFile(file);
         expect(fileType).toBe(DocumentTypeEnum.IMAGE);
       });
 
       it(`doit ramener PDF si le fichier est de type pdf`, () => {
-        let arrayOfBlob = new Array<Blob>();
-        let file = new File( arrayOfBlob, 'test-file.pdf', { type: 'application/pdf'});
+        const arrayOfBlob = new Array<Blob>();
+        const file = new File( arrayOfBlob, 'test-file.pdf', { type: 'application/pdf'});
         const fileType = documentManagerComponent.getFileTypeFromFile(file);
         expect(fileType).toBe(DocumentTypeEnum.IMAGE);
       });
 
       it(`doit ramener OTHER si le fichier est ni de type pdf ni de type image`, () => {
-        let arrayOfBlob = new Array<Blob>();
-        let file = new File( arrayOfBlob, 'test-file.zip', { type: 'application/zip'});
+        const arrayOfBlob = new Array<Blob>();
+        const file = new File( arrayOfBlob, 'test-file.zip', { type: 'application/zip'});
         const fileType = documentManagerComponent.getFileTypeFromFile(file);
         expect(fileType).toBe(DocumentTypeEnum.IMAGE);
       });
