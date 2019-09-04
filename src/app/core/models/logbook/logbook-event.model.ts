d905110bb5a7a7cb8ed37fc2be0979ceb109e046
T329354 <remazouari-ext@airfrance.fr>
2019-09-04 09:34:05 +0200

    EDOSPNC-1059 Affichage du service des rédacteurs CCO/ISCV dans la liste  des events JDB

--- src/app/core/models/logbook/logbook-event.model.ts
+++ src/app/core/models/logbook/logbook-event.model.ts
@@ -1,10 +1,10 @@
-import { DocumentModel } from './../document.model';
-import { LogbookEventModeEnum } from './../../enums/logbook-event/logbook-event-mode.enum';
-import { LogbookEventNotifiedEmail } from './logbook-event-notified-email.model';
-import { PncLightModel } from './../pnc-light.model';
-import { PncModel } from './../pnc.model';
-import { LogbookEventCategory } from './logbook-event-category';
+import { LogbookEventModeEnum } from '../../enums/logbook-event/logbook-event-mode.enum';
+import { DocumentModel } from '../document.model';
 import { EDossierPncObjectModel } from '../e-dossier-pnc-object.model';
+import { PncLightModel } from '../pnc-light.model';
+import { LogbookEventCategory } from './logbook-event-category';
+import { LogbookEventNotifiedEmail } from './logbook-event-notified-email.model';
+
 export class LogbookEventModel extends EDossierPncObjectModel {
     pnc: PncLightModel;
     redactor: PncLightModel;
@@ -23,6 +23,8 @@ export class LogbookEventModel extends EDossierPncObjectModel {
     notifiedRecipients: LogbookEventNotifiedEmail[];
     attachmentFiles: Array<DocumentModel> = new Array();
     mode: LogbookEventModeEnum;
+    type: string;
+    ccoGroupId: number;
 
     getStorageId(): string {
         return `${this.techId}`;
