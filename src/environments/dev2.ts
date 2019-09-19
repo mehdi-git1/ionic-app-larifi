435f50d7db24d7c93e40327ce75fdc1ef529470f
Ludovic CHEMIR <luchemir-ext@airfrance.com>
2019-09-19 16:15:39 +0200

    Fix Url pour envs DEV2 & RCT2

--- src/environments/dev2.ts
+++ src/environments/dev2.ts
@@ -10,7 +10,7 @@ export class Config extends BaseEnvironment {
         super();
 
         if (undefined !== window.cordova && 'browser' !== window.cordova.platformId) {
-            this.contextRoot = 'https://edospnc-api-aux-dev.airfrance.fr/api/';
+            this.contextRoot = 'https://edospnc-api-fix-dev.airfrance.fr/api/';
         }
 
         this.backEndUrl = `${this.contextRoot}rest/resources`;
