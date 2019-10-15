143c6f98c6fa863bddd06a7e96a1b622d5465a1b
Ludovic CHEMIR <luchemir-ext@airfrance.com>
2019-10-15 14:02:20 +0200

    1.11.0 -> 1.12.0

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '1.11.0';
+  public appVersion = '1.12.0';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public backEndUrl;
