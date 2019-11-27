da6469a033a3b3228cd66f16b7b9753fdf295cb8
T329354 <remazouari-ext@airfrance.fr>
2019-11-27 17:01:09 +0100

    EDOSPNC-1328 Revue PR

--- src/app/core/enums/app-parameter.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum AppParameterEnum {
-    'EFORMS_LINK' = 'EFORMS_LINK',
-    'DOMINO_LINK' = 'DOMINO_LINK',
-    'EDITO_LINK' = 'EDITO_LINK',
-    'CIRCUITPN_LINK' = 'CIRCUITPN_LINK'
-}
--- src/app/core/models/statutory-certificate/career-history.model.ts
+++
@@ -1,4 +0,0 @@
-export class CareerHistoryModel {
-    label: string;
-    link: string;
-}
