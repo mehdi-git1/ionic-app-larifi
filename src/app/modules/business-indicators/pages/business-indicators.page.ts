a725332984f5a737209aac6783bf34c4b2c3a180
Ludovic CHEMIR <luchemir-ext@airfrance.com>
2019-12-11 18:57:38 +0100

    Fix Tslint

--- src/app/modules/business-indicators/pages/business-indicators.page.ts
+++ src/app/modules/business-indicators/pages/business-indicators.page.ts
@@ -1,5 +1,8 @@
+import { PncService } from './../../../core/services/pnc/pnc.service';
+import { PncModel } from './../../../core/models/pnc.model';
 import { TabHeaderEnum } from 'src/app/core/enums/tab-header.enum';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'page-business-indicators',
@@ -8,4 +11,12 @@ import { Component } from '@angular/core';
 })
 export class BusinessIndicatorsPage {
     TabHeaderEnum = TabHeaderEnum;
+    pnc: PncModel;
+
+    constructor(private activatedRoute: ActivatedRoute, private pncService: PncService) {
+        const matricule = this.pncService.getRequestedPncMatricule(this.activatedRoute);
+        this.pncService.getPnc(matricule).then(pnc => {
+            this.pnc = pnc;
+        }, error => { });
+    }
 }
