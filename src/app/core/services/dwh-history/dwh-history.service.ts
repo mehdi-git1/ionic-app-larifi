e21394d69fb9a035c22dd4786a6bd0f4f097493a
Thibaut Dumont <>
2020-01-03 12:30:40 +0100

    EDOSPNC-1368 Gestion de l'historique DWH hors ligne

--- src/app/core/services/dwh-history/dwh-history.service.ts
+++ src/app/core/services/dwh-history/dwh-history.service.ts
@@ -1,16 +1,29 @@
+import { ConnectivityService } from './../connectivity/connectivity.service';
+import { OfflineDwhHistoryService } from './offline-dwh-history.service';
+import { OnlineDwhHistoryService } from './online-dwh-history.service';
 import { DwhHistoryModel } from './../../models/dwh-history/dwh-history.model';
 import { Injectable } from '@angular/core';
 
 import { UrlConfiguration } from '../../configuration/url.configuration';
 import { RestService } from '../../http/rest/rest.base.service';
+import { BaseService } from '../base/base.service';
 
 @Injectable({ providedIn: 'root' })
-export class DwhHistoryService {
+export class DwhHistoryService extends BaseService {
 
     constructor(
         public restService: RestService,
-        public config: UrlConfiguration
-    ) { }
+        public config: UrlConfiguration,
+        public connectivityService: ConnectivityService,
+        public onlineDwhHistoryService: OnlineDwhHistoryService,
+        public offlineDwhHistoryService: OfflineDwhHistoryService
+    ) { 
+        super(
+            connectivityService,
+            onlineDwhHistoryService,
+            offlineDwhHistoryService
+        );
+    }
 
     /**
      * Récupère l'historique DWH du Pnc
@@ -18,6 +31,6 @@ export class DwhHistoryService {
      * @return une promesse contenant l'historique DWH du Pnc
      */
     public getDwhHistory(matricule: string): Promise<DwhHistoryModel> {
-        return this.restService.get(this.config.getBackEndUrl('getDwhHistory', [matricule]));
+        return this.execFunctionService('getDwhHistory', matricule);
     }
 }
---
+++ src/app/core/services/dwh-history/offline-dwh-history.service.ts
@@ -0,0 +1,23 @@
+import { DwhHistoryModel } from '../../models/dwh-history/dwh-history.model';
+import { Injectable } from '@angular/core';
+
+import { UrlConfiguration } from '../../configuration/url.configuration';
+import { RestService } from '../../http/rest/rest.base.service';
+
+@Injectable({ providedIn: 'root' })
+export class OfflineDwhHistoryService {
+
+    constructor(
+        public restService: RestService,
+        public config: UrlConfiguration
+    ) { }
+
+    /**
+     * Récupère l'historique DWH du Pnc hors ligne
+     * @param matricule matricule du Pnc
+     * @return une promesse contenant null tant que l'historique DWH n'est pas disponible hors lign
+     */
+    public getDwhHistory(matricule: string): Promise<DwhHistoryModel> {
+        return Promise.resolve(null);
+    }
+}
