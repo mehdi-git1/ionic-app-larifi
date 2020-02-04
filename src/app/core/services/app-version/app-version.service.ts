8d2c7da3748c46456edd6a8c5c8c284688b2e9ce
M429676 <paramane@airfrance.fr>
2020-02-04 14:34:48 +0100

    EDOSPNC-1371 suppr le lien vers l'admin

--- src/app/core/services/app-version/app-version.service.ts
+++ src/app/core/services/app-version/app-version.service.ts
@@ -28,22 +28,4 @@ export class AppVersionService {
     public getAppVersionById(id: number): Promise<AppVersionModel> {
         return this.restService.get(this.config.getBackEndUrl('getAppVersionById', [id]));
     }
-
-    /**
-     * Crée ou met à jour une version
-     * @param appVersion la version à créer ou à mettre à jour
-     * @return la version créée ou mise à jour
-     */
-    public createOrUpdateAppVersion(appVersion: AppVersionModel): Promise<AppVersionModel> {
-        return this.restService.post(this.config.getBackEndUrl('appVersions'), appVersion);
-    }
-
-    /**
-     * Supprime une version
-     * @param id l'id de la version à supprimer
-     * @return une promesse qui se résout quand la suppression est faite
-     */
-    delete(id: number): Promise<void> {
-        return this.restService.delete(this.config.getBackEndUrl('deleteAppVersionById', [id]));
-    }
 }
