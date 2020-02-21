375d999759630e7358b174c59d0167370539b81a
M429676 <paramane@airfrance.fr>
2020-02-21 14:51:02 +0100

    EDOSPNC-1446 retrait des disabled dans les filtres

--- src/app/modules/pnc-team/components/pnc-search-filter/pnc-search-filter.component.ts
+++ src/app/modules/pnc-team/components/pnc-search-filter/pnc-search-filter.component.ts
@@ -1,7 +1,7 @@
 import { CareerObjectiveCategory } from 'src/app/core/models/career-objective-category';
 
 import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Events, PopoverController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -174,7 +174,6 @@ export class PncSearchFilterComponent implements AfterViewInit {
     this.searchForm.get('prioritizedControl').setValue(false);
     this.searchForm.get('hasAtLeastOnePriorityInProgressControl').setValue(false);
     this.searchForm.get('hasNoPriorityControl').setValue(false);
-    this.searchForm.get('priorityControl').setValue(new Array());
     this.searchForm.get('hasDefaultHiddenEventsControl').setValue(false);
     this.searchForm.get('hasHiddenEventsControl').setValue(false);
     this.search();
@@ -192,20 +191,55 @@ export class PncSearchFilterComponent implements AfterViewInit {
       SpecialityEnum.ALT
       : (this.pncFilter.speciality ? this.pncFilter.speciality : AppConstant.ALL);
 
-    this.searchForm = this.formBuilder.group({
-      divisionControl: [this.pncFilter.division ? this.pncFilter.division : AppConstant.ALL],
-      sectorControl: [this.pncFilter.sector ? this.pncFilter.sector : AppConstant.ALL],
-      ginqControl: [this.pncFilter.ginq ? this.pncFilter.ginq : AppConstant.ALL],
-      specialityControl: [specialityInitValue],
-      aircraftSkillControl: [this.pncFilter.aircraftSkill ? this.pncFilter.aircraftSkill : AppConstant.ALL],
-      relayControl: [this.pncFilter.relay ? this.pncFilter.relay : AppConstant.ALL],
-      priorityCategoryControl: [this.pncFilter ? this.pncFilter.priorityCategoryCode : AppConstant.ALL],
-      priorityControl: [new Array()],
-      prioritizedControl: [false],
-      hasAtLeastOnePriorityInProgressControl: [false],
-      hasNoPriorityControl: [false],
-      hasDefaultHiddenEventsControl: [false],
-      hasHiddenEventsControl: [false]
+    this.searchForm = new FormGroup({
+      divisionControl: new FormControl({
+        value: [this.pncFilter.division ? this.pncFilter.division : AppConstant.ALL],
+        disabled: this.areFiltersDisabled()
+      }),
+      sectorControl: new FormControl({
+        value: [this.pncFilter.sector ? this.pncFilter.sector : AppConstant.ALL],
+        disabled: this.areFiltersDisabled()
+      }),
+      ginqControl: new FormControl({
+        value: [this.pncFilter.ginq ? this.pncFilter.ginq : AppConstant.ALL],
+        disabled: this.areFiltersDisabled()
+      }),
+      specialityControl: new FormControl({
+        value: [specialityInitValue],
+        disabled: this.areFiltersDisabled()
+      }),
+      aircraftSkillControl: new FormControl({
+        value: [this.pncFilter.aircraftSkill ? this.pncFilter.aircraftSkill : AppConstant.ALL],
+        disabled: this.areFiltersDisabled()
+      }),
+      relayControl: new FormControl({
+        value: [this.pncFilter.relay ? this.pncFilter.relay : AppConstant.ALL],
+        disabled: this.areFiltersDisabled()
+      }),
+      priorityCategoryControl: new FormControl({
+        value: [this.pncFilter ? this.pncFilter.priorityCategoryCode : AppConstant.ALL],
+        disabled: this.areFiltersDisabled()
+      }),
+      prioritizedControl: new FormControl({
+        value: [false],
+        disabled: this.noPriority || this.areFiltersDisabled()
+      }),
+      hasAtLeastOnePriorityInProgressControl: new FormControl({
+        value: [false],
+        disabled: this.noPriority || this.areFiltersDisabled()
+      }),
+      hasNoPriorityControl: new FormControl({
+        value: [false],
+        disabled: this.prioritized || this.noPriority || this.areFiltersDisabled()
+      }),
+      hasDefaultHiddenEventsControl: new FormControl({
+        value: [false],
+        disabled: this.areFiltersDisabled()
+      }),
+      hasHiddenEventsControl: new FormControl({
+        value: [false],
+        disabled: this.areFiltersDisabled()
+      })
     });
     if (this.connectivityService.isConnected()) {
       this.resetFilterValues();
