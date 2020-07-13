749d1fd4c880f76803311744f673a1795ba995ca
Dumont <m414147@airfrance.fr>
2020-07-13 10:10:08 +0200

    EDSOPNC-1534 Replace spinner ionic par angular

--- src/app/shared/components/edossier-spinner/edossier-spinner.component.ts
+++ src/app/shared/components/edossier-spinner/edossier-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'edossier-spinner',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class EdossierSpinnerComponent {
+
+  @Input() verticalMargin = 0;
+  @Input() diameter = 100;
+
+  constructor() { }
+
 }
