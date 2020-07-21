b2f73541765fb90ee774c1562492ff224899e464
Ramane <M429676@airfrance.fr>
2020-07-21 18:15:25 +0200

    EDOSPNC-1477 lien vers my board

--- src/app/modules/my-board/my-board.module.ts
+++ src/app/modules/my-board/my-board.module.ts
@@ -1,20 +1,32 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { MyBoardNotificationsListComponent } from './components/my-board-notifications-list/my-board-notifications-list.component';
-import { IonicModule } from '@ionic/angular';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { ComponentsModule } from 'src/app/shared/components/components.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
-
+import {
+    MyBoardNotificationsListComponent
+} from './components/my-board-notifications-list/my-board-notifications-list.component';
+import { MyBoardHomePage } from './pages/my-board-home/my-board-home.page';
 
 @NgModule({
-  declarations: [MyBoardNotificationsListComponent],
+  declarations: [
+    MyBoardNotificationsListComponent,
+    MyBoardHomePage
+  ],
   imports: [
     IonicModule,
     SharedModule,
     ComponentsModule,
     FormsModule,
     ReactiveFormsModule
+  ],
+  entryComponents: [
+    MyBoardHomePage
+  ],
+  exports: [
+    MyBoardHomePage
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
