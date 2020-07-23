cc2877589a3fc8fd68b19287343fc43bd074545a
caso <caso853@gmail.com>
2020-07-23 10:31:15 +0200

    EDOSPNC-1535 affichage liste notification

--- src/app/modules/my-board/components/my-board-notifications-list/my-board-notifications-list.component.ts
+++ src/app/modules/my-board/components/my-board-notifications-list/my-board-notifications-list.component.ts
@@ -1,18 +1,34 @@
+import { TranslateService } from '@ngx-translate/core';
+import { NotificationDocumentTypeEnum } from './../../../../core/enums/my-board/notification-document-type.enum';
 import { Component, OnInit, Input } from '@angular/core';
 import { MyBoardNotificationModel } from 'src/app/core/models/my-board/my-board-notification.model';
+import { MenuController } from '@ionic/angular';
+import { DocumentTypeEnum } from 'src/app/core/models/document.model';
 
 @Component({
-  selector: 'app-my-board-notifications-list',
+  selector: 'my-board-notifications-list',
   templateUrl: './my-board-notifications-list.component.html',
   styleUrls: ['./my-board-notifications-list.component.scss'],
 })
 export class MyBoardNotificationsListComponent implements OnInit {
 
   @Input()
-  notificationList: MyBoardNotificationModel[];
+  notificationList: MyBoardNotificationModel[] = [];
 
-  constructor() { }
+  constructor(private translateService: TranslateService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+  }
+
+  /**
+   * Récupère le libellé du type document dans la locale
+   * @param documentType type de document concerné par la notification
+   * @return la traduction
+   */
+  getDocumentTypeTranslation(documentType: NotificationDocumentTypeEnum): string {
+    const name = NotificationDocumentTypeEnum[documentType];
+    return this.translateService.instant('MY_BOARD.DOCUMENT_TYPE.'.concat(name));
+  }
 
 }
--- src/app/modules/my-board/pages/my-board-home/my-board-home.page.ts
+++ src/app/modules/my-board/pages/my-board-home/my-board-home.page.ts
@@ -1,4 +1,11 @@
+import { AppConstant } from './../../../../app.constant';
+import { MyBoardNotificationFilterModel } from './../../../../core/models/my-board/my-board-notification-filter.model';
+import { MyBoardNotificationModel } from 'src/app/core/models/my-board/my-board-notification.model';
+import { MyBoardNotificationService } from './../../../../core/services/my-board/my-board-notification.service';
 import { Component, OnInit } from '@angular/core';
+import { ConnectivityService } from 'src/app/core/services/connectivity/connectivity.service';
+import { PncModel } from 'src/app/core/models/pnc.model';
+import { SecurityService } from 'src/app/core/services/security/security.service';
 
 @Component({
   selector: 'my-board-home',
@@ -6,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-board-home.page.scss'],
 })
 export class MyBoardHomePage implements OnInit {
+  pnc: PncModel;
+  pncNotifications: MyBoardNotificationModel[];
+  filter: MyBoardNotificationFilterModel;
 
-  constructor() { }
+  constructor(private securityService: SecurityService, private myBoardNotificationService: MyBoardNotificationService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.securityService.getAuthenticatedUser().then(authenticated => {
+      this.pnc = authenticated.authenticatedPnc;
+
+      this.filter = new MyBoardNotificationFilterModel();
+      this.filter.notifiedPncMatricule = '42414258';
+
+      this.filter.offset = 0;
+      this.filter.page = 0;
+      this.filter.size = AppConstant.pageSize;
+      this.myBoardNotificationService.getNotifications(this.filter).then(pagedNotification => {
+        this.pncNotifications = pagedNotification.content;
+      });
+    });
+
+  }
 
 }
