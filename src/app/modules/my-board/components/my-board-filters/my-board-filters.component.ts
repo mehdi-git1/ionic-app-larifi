f467522529352229c836869df6ce55fd49699604
Dumont <m414147@airfrance.fr>
2020-08-06 13:39:18 +0200

    EDOSPNC-1479 Ajout d'un mat-select à la place du ion-select

--- src/app/modules/my-board/components/my-board-filters/my-board-filters.component.ts
+++ src/app/modules/my-board/components/my-board-filters/my-board-filters.component.ts
@@ -1,60 +1,96 @@
 import {
-  MyBoardNotificationFilterModel
+    MyBoardNotificationFilterModel
 } from 'src/app/core/models/my-board/my-board-notification-filter.model';
 
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
-import {
-  NotificationDocumentTypeEnum
-} from '../../../../core/enums/my-board/notification-document-type.enum';
 import { SessionService } from '../../../../core/services/session/session.service';
 
 @Component({
-  selector: 'my-board-filters',
-  templateUrl: './my-board-filters.component.html',
-  styleUrls: ['./my-board-filters.component.scss'],
+    selector: 'my-board-filters',
+    templateUrl: './my-board-filters.component.html',
+    styleUrls: ['./my-board-filters.component.scss'],
 })
 export class MyBoardFiltersComponent implements OnInit {
 
-  @Input() filters: MyBoardNotificationFilterModel;
-  @Output() filtersChanged = new EventEmitter<MyBoardNotificationFilterModel>();
+    @Input() filters: MyBoardNotificationFilterModel;
+    @Output() filtersChanged = new EventEmitter<MyBoardNotificationFilterModel>();
 
-  filterForm: FormGroup;
+    filterForm: FormGroup;
 
-  documentTypes: Set<NotificationDocumentTypeEnum>;
+    documentTypes: Array<any>;
 
-  constructor(
-    private sessionService: SessionService,
-    private formBuilder: FormBuilder
+    constructor(
+        private sessionService: SessionService,
+        private formBuilder: FormBuilder,
+        private translateService: TranslateService
 
-  ) {
-    this.documentTypes = this.sessionService.getActiveUser().appInitData.myBoardInitData.notificationDocumentTypes;
+    ) {
+        this.documentTypes = this.initDocumentTypes();
 
-    this.initForm();
-  }
-
-  ngOnInit() {
-  }
-
-  /**
-   * Initialise le formulaire
-   */
-  initForm() {
-    this.filterForm = this.formBuilder.group({
-      documentTypes: [''],
-    });
-  }
-
-  /**
-   * Déclenché quand les types de document sont mis à jour
-   * @param event l'événement déclenché
-   */
-  documentTypesChanged(event) {
-    this.filters.documentTypes = new Array();
-    for (const documentType of event.detail.value) {
-      this.filters.documentTypes.push(documentType);
+        this.initForm();
     }
-    this.filtersChanged.emit();
-  }
+
+    ngOnInit() {
+    }
+
+    /**
+     * Initialise le formulaire
+     */
+    initForm() {
+        this.filterForm = this.formBuilder.group({
+            documentTypes: [''],
+        });
+    }
+
+    /**
+     * Initialise la liste des types de document
+     * @return la liste initialisée
+     */
+    initDocumentTypes(): Array<any> {
+        const documentTypeArray = new Array();
+        const documentTypes = this.sessionService.getActiveUser().appInitData.myBoardInitData.notificationDocumentTypes;
+        for (const documentType of documentTypes) {
+            documentTypeArray.push({
+                value: documentType,
+                label: this.translateService.instant('MY_BOARD.DOCUMENT_TYPE.' + documentType)
+            });
+        }
+        return this.sortDocumentTypes(documentTypeArray);
+    }
+
+    /**
+     * Tri une liste de type de document par ordre alphabétique
+     * @param documentTypes la liste des documents à trier
+     * @return la liste triée
+     */
+    sortDocumentTypes(documentTypes: Array<any>): Array<any> {
+        documentTypes.sort((documentType1, documentType2) => {
+            if (documentType1.label.toLowerCase() < documentType2.label.toLowerCase()) {
+                return -1;
+            }
+            if (documentType1.label.toLowerCase() > documentType2.label.toLowerCase()) {
+                return 1;
+            }
+            return 0;
+        });
+        return documentTypes;
+    }
+
+    /**
+     * Déclenché quand les types de document sont mis à jour
+     * @param event l'événement déclenché
+     */
+    documentTypesChanged(event) {
+        if (event.value !== undefined) {
+            this.filters.documentTypes = new Array();
+            for (const documentType of event.value) {
+                this.filters.documentTypes.push(documentType);
+            }
+            this.filtersChanged.emit();
+        }
+    }
+
 }
