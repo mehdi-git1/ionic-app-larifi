6db9816be5bcbea6d79c53bb50e6e6a6b166efd3
T329354 <remazouari-ext@airfrance.fr>
2020-10-15 13:07:09 +0200

    Mise a jour de la version 2.4.0 -> 2.5.0

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '2.4.0';
+  public appVersion = '2.5.0';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public backEndUrl;
