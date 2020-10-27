563180878a766f725626b669b48c846cc6e3c399
Mazouari <T329354@airfrance.fr>
2020-10-27 09:42:24 +0100

    EDOSPNC-1589 Revue PR

--- src/app/core/models/pnc-filter.model.ts
+++ src/app/core/models/pnc-filter.model.ts
@@ -3,29 +3,29 @@ import { SortDirection } from '../enums/sort-direction-enum';
 
 export class PncFilterModel {
 
-    // Filtres
-    pncMatricule: string;
-    division: string;
-    sector: string;
-    ginq: string;
-    speciality: string;
-    aircraftSkill: string;
-    relay: string;
-    prioritized: boolean;
-    hasAtLeastOnePriorityInProgress: boolean;
-    hasNoPriority: boolean;
-    hasHiddenEvents: boolean;
-    hasDefaultHiddenEvents: boolean;
-    priorityCategoryCode: string;
-    hasPIOrEPPGreaterThan24Months: boolean;
-    hasManifex: boolean;
-    taf: boolean;
-    workRate: number;
+  // Filtres
+  pncMatricule: string;
+  division: string;
+  sector: string;
+  ginq: string;
+  speciality: string;
+  aircraftSkill: string;
+  relay: string;
+  prioritized: boolean;
+  hasAtLeastOnePriorityInProgress: boolean;
+  hasNoPriority: boolean;
+  hasHiddenEvents: boolean;
+  hasDefaultHiddenEvents: boolean;
+  priorityCategoryCode: string;
+  hasPIOrEPPGreaterThan24Months: boolean;
+  hasManifex: boolean;
+  taf: boolean;
+  workRate: number;
 
-    // Tri
-    sortColumn: string;
-    sortDirection: SortDirection;
-  
+  // Tri
+  sortColumn: string;
+  sortDirection: SortDirection;
+
   // Page
   size: number;
   page: number;
