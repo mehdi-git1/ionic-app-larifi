3cc3fe9467eda5d2975c5a90fb731a8295785972
m426155 <mccamara@airfrance.fr>
2021-02-17 16:01:45 +0100

    EDOSPNC-1660 correction bug ouverture sur IOS

--- src/app/core/file/html/html.service.ts
+++ src/app/core/file/html/html.service.ts
@@ -4,15 +4,15 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 @Injectable({ providedIn: 'root' })
 export class HtmlService {
 
-    constructor(
-        private inAppBrowser: InAppBrowser) {
-    }
+  constructor(
+    private inAppBrowser: InAppBrowser) {
+  }
 
-    /**
-     * Ouvre une fenêtre de navigation avec l'url concernée
-     * @param url  : url de la fiche synthèse concernée
-     */
-    displayHTML(url) {
-        this.inAppBrowser.create(url, '_system', '');
-    }
+  /**
+   * Ouvre une fenêtre de navigation avec l'url concernée
+   * @param url  : url de la fiche synthèse concernée
+   */
+  displayHTML(url) {
+    this.inAppBrowser.create(url, '_system', { usewkwebview: 'yes' }).show();;
+  }
 }
