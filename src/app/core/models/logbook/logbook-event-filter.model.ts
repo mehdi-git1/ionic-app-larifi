b49d1360b2862e0fbe20343bea54703e04fd69f0
Camara <M426155@airfrance.fr>
2021-07-28 19:53:08 +0200

    EDOSPNC-1695 pagination JDB

---
+++ src/app/core/models/logbook/logbook-event-filter.model.ts
@@ -0,0 +1,18 @@
+import { LogbookEventStatusEnum } from "../../enums/logbook-event/logbook-event-status-enum";
+import { SortDirection } from "../../enums/sort-direction-enum";
+
+export class LogbookEventFilterModel {
+  matricule: string;
+  status: LogbookEventStatusEnum;
+  isLastEvent: boolean;
+  groupId: number;
+  archived: boolean;
+  // sort
+  sortColumn: string;
+  sortDirection: SortDirection;
+
+  // Pagination
+  size: number;
+  offset: number;
+  page: number;
+}
