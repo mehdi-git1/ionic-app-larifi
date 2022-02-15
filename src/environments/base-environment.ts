8d20c7312cb9e382976ded858e29c1a08120bb32
m426155 <mccamara@airfrance.fr>
2022-02-15 14:42:32 +0100

    montée version

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '3.3.2';
+  public appVersion = '3.3.3';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public backEndUrl;
