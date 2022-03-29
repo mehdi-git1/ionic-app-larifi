9146ddf244ac2c303fecbd1ebecb764a73e61cf9
t329354 <t329354@airfrance.fr>
2022-03-29 11:40:28 +0200

    Fix probleme affichage langue dans le detail des EDP

--- src/app/modules/professional-interview/pages/professional-interview-details/professional-interview-details.page.ts
+++ src/app/modules/professional-interview/pages/professional-interview-details/professional-interview-details.page.ts
@@ -9,24 +9,24 @@ import { TranslateService } from '@ngx-translate/core';
 
 import { PncRoleEnum } from '../../../../core/enums/pnc-role.enum';
 import {
-  ProfessionalInterviewCommentItemTypeEnum
+    ProfessionalInterviewCommentItemTypeEnum
 } from '../../../../core/enums/professional-interview/professional-interview-comment-item-type.enum';
 import {
-  ProfessionalInterviewConditionEnum
+    ProfessionalInterviewConditionEnum
 } from '../../../../core/enums/professional-interview/professional-interview-condition.enum';
 import {
-  ProfessionalInterviewStateEnum
+    ProfessionalInterviewStateEnum
 } from '../../../../core/enums/professional-interview/professional-interview-state.enum';
 import {
-  ProfessionalInterviewTypeEnum
+    ProfessionalInterviewTypeEnum
 } from '../../../../core/enums/professional-interview/professional-interview-type.enum';
 import { DocumentModel } from '../../../../core/models/document.model';
 import { PncModel } from '../../../../core/models/pnc.model';
 import {
-  ProfessionalInterviewThemeModel
+    ProfessionalInterviewThemeModel
 } from '../../../../core/models/professional-interview/professional-interview-theme.model';
 import {
-  ProfessionalInterviewModel
+    ProfessionalInterviewModel
 } from '../../../../core/models/professional-interview/professional-interview.model';
 import { ConnectivityService } from '../../../../core/services/connectivity/connectivity.service';
 import { DeviceService } from '../../../../core/services/device/device.service';
@@ -34,13 +34,13 @@ import { OfflinePncService } from '../../../../core/services/pnc/offline-pnc.ser
 import { PncTransformerService } from '../../../../core/services/pnc/pnc-transformer.service';
 import { PncService } from '../../../../core/services/pnc/pnc.service';
 import {
-  OfflineProfessionalInterviewService
+    OfflineProfessionalInterviewService
 } from '../../../../core/services/professional-interview/offline-professional-interview.service';
 import {
-  ProfessionalInterviewStatusService
+    ProfessionalInterviewStatusService
 } from '../../../../core/services/professional-interview/professional-interview-status.service';
 import {
-  ProfessionalInterviewService
+    ProfessionalInterviewService
 } from '../../../../core/services/professional-interview/professional-interview.service';
 import { SecurityService } from '../../../../core/services/security/security.service';
 import { SessionService } from '../../../../core/services/session/session.service';
@@ -164,6 +164,7 @@ export class ProfessionalInterviewDetailsPage {
 
         this.pncService.getPnc(this.professionalInterview.matricule).then(pnc => {
           this.pnc = pnc;
+          this.professionalInterview.pncAtInterviewDate = this.pncTransformer.toPncLight(this.pnc);
         }, error => { });
         this.editionMode = this.isEditable();
         this.isPncCommentEditable = (this.isConcernedPnc()
