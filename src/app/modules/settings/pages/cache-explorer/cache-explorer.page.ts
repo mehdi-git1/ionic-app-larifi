59193b6cc92b02b271663eacc761043e6db6d7f0
t884015 <modiagne-ext@airfrance.fr>
2022-04-13 16:45:07 +0200

    EDOSPNC-1682 : Ajout de limit pour les données à affichier

--- src/app/modules/settings/pages/cache-explorer/cache-explorer.page.ts
+++ src/app/modules/settings/pages/cache-explorer/cache-explorer.page.ts
@@ -34,7 +34,12 @@ export class CacheExplorerPage implements OnInit {
       this.dataFromCache = cacheData;
       this.data = cacheData;
       delete this.dataFromCache[EntityEnum.PNC_PHOTO]
-      if ((Utils.getSizeOfObject(this.dataFromCache[EntityEnum.PNC]) > AppConstant.ENTITY_CACHE_SIZE)) {
+      if ((Utils.getSizeOfObject(this.dataFromCache[EntityEnum.PNC]) > AppConstant.ENTITY_CACHE_SIZE)
+        || (Utils.getSizeOfObject(this.dataFromCache[EntityEnum.PROFESSIONAL_INTERVIEW]) > AppConstant.ENTITY_CACHE_SIZE)
+        || (Utils.getSizeOfObject(this.dataFromCache[EntityEnum.PROFESSIONAL_LEVEL]) > AppConstant.ENTITY_CACHE_SIZE)
+        || (Utils.getSizeOfObject(this.dataFromCache[EntityEnum.CAREER_OBJECTIVE]) > AppConstant.ENTITY_CACHE_SIZE)
+        || (Utils.getSizeOfObject(this.dataFromCache[EntityEnum.WAYPOINT]) > AppConstant.ENTITY_CACHE_SIZE)
+        || (Utils.getSizeOfObject(this.dataFromCache[EntityEnum.USER_MESSAGE]) > AppConstant.ENTITY_CACHE_SIZE)) {
         this.selectedItem = EntityEnum.AUTHENTICATED_USER;
         this.dataFromCache = this.dataFromCache[this.selectedItem];
         this.message = true;
