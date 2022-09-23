7457ba51305958ef7c9718fc5e1c09269b591e7f
t884015 <modiagne-ext@airfrance.fr>
2022-09-23 10:53:24 +0200

    EDOSPNC-1825 : Synchroniser le PNC quand il n'est pas dans le cache

--- src/app/core/services/synchronization/synchronization.service.ts
+++ src/app/core/services/synchronization/synchronization.service.ts
@@ -97,27 +97,31 @@ export class SynchronizationService {
         } else {
           let pncToSynchronise = this.storageService.findOne(EntityEnum.PNC, matricule);
           this.pncService.getPnc(matricule).then(pnc => {
-            //const isToUpdate = moment(pncToSynchronise.offlineStorageDate, AppConstant.isoDateFormat).isBefore(moment(pnc.metadataDate.lastPncProfessionalFileUpdateDate, AppConstant.isoDateFormat));
-            if (pncToSynchronise && moment(pncToSynchronise.offlineStorageDate, AppConstant.isoDateFormat).isBefore(moment(pnc.metadataDate.lastPncProfessionalFileUpdateDate, AppConstant.isoDateFormat))) {
-              this.pncSynchroProvider.getPncSynchro(matricule).then(pncSynchro => {
-                this.updateLocalStorageFromPncSynchroResponse(pncSynchro);
-                resolve(true);
-              }, error => {
-                reject(error.detailMessage);
-              });
-            }
-            // Le PNC n'existe pas dans le cache
             if (!pncToSynchronise) {
+              // Le PNC n'existe pas dans le cache
               this.pncSynchroProvider.getPncSynchro(matricule).then(pncSynchro => {
-                this.storageService.save(EntityEnum.PNC, this.pncTransformer.toPnc(pncSynchro.pnc), true);
                 this.updateLocalStorageFromPncSynchroResponse(pncSynchro);
                 resolve(true);
               }, error => {
                 reject(error.detailMessage);
               });
+            } else {
+              // Le PNC existe dans le cache
+              const isToUpdate = moment(pncToSynchronise.offlineStorageDate, AppConstant.isoDateFormat).isBefore(moment(pnc.metadataDate.lastPncProfessionalFileUpdateDate, AppConstant.isoDateFormat));
+              if (pncToSynchronise && isToUpdate) {
+                this.pncSynchroProvider.getPncSynchro(matricule).then(pncSynchro => {
+                  this.updateLocalStorageFromPncSynchroResponse(pncSynchro);
+                  resolve(true);
+                }, error => {
+                  reject(error.detailMessage);
+                });
+              }
+              // Si il n'y a pas de changement en BD
+              if (pncToSynchronise && !isToUpdate) {
+                // Les données en BD sont bien synchronisées
+                resolve(true)
+              }
             }
-
-
           });
         }
       } else {
