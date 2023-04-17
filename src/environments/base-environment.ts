966fe75b068cc8cffc1a83c9d9396deff6671d12
t884015 <modiagne-ext@airfrance.fr>
2023-04-17 15:49:31 +0200

    Montée de version 3.8.2

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '3.8.1';
+  public appVersion = '3.8.2';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public appScheme = "edossier://secmobil"
