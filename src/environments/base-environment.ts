4debb6ac733f0bf6c8aa7bf2d268712de98909d6
t884015 <modiagne-ext@airfrance.fr>
2023-06-05 11:39:42 +0200

    montée de version 3.8.3 et regénération package-lock.json

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '3.8.2';
+  public appVersion = '3.8.3';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public appScheme = "edossier://secmobil"
