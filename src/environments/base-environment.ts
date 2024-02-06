bf0e6681c0d744dbd77b19e5c5c8309cebbb1444
Mchouak <T582107@airfrance.fr>
2024-02-06 10:36:48 +0100

    fix num version

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '3.10.0';
+  public appVersion = '3.10.1';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public appScheme = "edossier://secmobil"
