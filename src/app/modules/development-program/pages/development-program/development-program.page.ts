ed518e899873b2c783734f604464df784aa3a1d2
Imane BOUHSINE <imbouhsine-ext@airfrance.fr>
2024-02-21 11:34:30 +0100

    feature/EDOSPNC-1992

--- src/app/modules/development-program/pages/development-program/development-program.page.ts
+++ src/app/modules/development-program/pages/development-program/development-program.page.ts
@@ -37,6 +37,7 @@ import {
     SynchronizationService
 } from '../../../../core/services/synchronization/synchronization.service';
 import { CareerObjectiveStatusEnum } from 'src/app/core/enums/career-objective-status.enum';
+import { AuthenticatedUserModel } from '../../../../core/models/authenticated-user.model';
 
 @Component({
     selector: 'page-development-program',
@@ -52,6 +53,7 @@ export class DevelopmentProgramPage {
     matricule: string;
     formsInputParam: FormsInputParamsModel;
     lastConsultedRotation: RotationModel;
+    authenticatedUser: AuthenticatedUserModel;
 
     canDisplayMenu = false;
 
@@ -100,6 +102,7 @@ export class DevelopmentProgramPage {
 
     ionViewDidEnter() {
         this.matricule = this.pncService.getRequestedPncMatricule(this.activatedRoute);
+        this.authenticatedUser = this.sessionService.getActiveUser();
         if (this.matricule !== null && this.matricule !== undefined) {
             this.pncService.getPnc(this.matricule).then(pnc => {
                 this.pnc = pnc;
@@ -109,6 +112,9 @@ export class DevelopmentProgramPage {
             this.getProfessionalInterviewList();
         }
     }
+
+
+
 
     /**
      * Récupére la liste des eObservations
@@ -226,7 +232,9 @@ export class DevelopmentProgramPage {
             result.sort((careerObjective: CareerObjectiveModel, otherCareerObjective: CareerObjectiveModel) => {
                 return careerObjective.creationDate < otherCareerObjective.creationDate ? 1 : -1;
             });
-            this.careerObjectives = result.filter(careerObjective => CareerObjectiveStatusEnum.DELETED !== careerObjective.careerObjectiveStatus);
+            this.careerObjectives = result.filter(careerObjective => (CareerObjectiveStatusEnum.DELETED !== careerObjective.careerObjectiveStatus) && !((careerObjective.careerObjectiveStatus == CareerObjectiveStatusEnum.DRAFT)
+            && (careerObjective.initiator == "MANAGER") && (this.authenticatedUser.authenticatedPnc.speciality != "CAD")));
+            
         }, error => { });
     }
 }
