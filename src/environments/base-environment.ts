68ccec222e19f036b8623150b299a67e0fa0dfe5
Imane BOUHSINE <imbouhsine-ext@airfrance.fr>
2024-04-29 12:13:56 +0200

    version 3.10.6

--- src/environments/base-environment.ts
+++ src/environments/base-environment.ts
@@ -1,6 +1,6 @@
 
 export abstract class BaseEnvironment {
-  public appVersion = '3.10.5';
+  public appVersion = '3.10.6';
   public contextRoot = '/api/';
   public appName = 'EDossierPnc';
   public appScheme = "edossier://secmobil"
